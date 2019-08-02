3d740cc770f28b2a6cb2abc51b7b32bdc453fc46
George Haberis <george.haberis@guardian.co.uk>
2019-08-02 14:41:15 +0100

    update tc-validate.sh script to install yarn

--- tc-validate.sh
+++ tc-validate.sh
@@ -1,9 +1,27 @@
 #!/bin/bash -xe
 
+set -e
+
+# Installing yarn
+
+YARN_VERSION="1.7.0"
+YARN_LOCATION="$(pwd)/tools/${YARN_VERSION}"
+
+if [ ! -d "$YARN_LOCATION" ]; then
+	mkdir -p ${YARN_LOCATION}
+	cd ${YARN_LOCATION}/
+	wget -qO- https://github.com/yarnpkg/yarn/releases/download/v${YARN_VERSION}/yarn-v${YARN_VERSION}.tar.gz | tar zvx
+	cd ../..
+fi
+
+export PATH="$PATH:$YARN_LOCATION/yarn-v$YARN_VERSION/bin"
+
+yarn -h
+
 # Install dependencies
-nvm install --save-dev yarn
-yarn
+# npm install --save-dev yarn
+# yarn
 
 # Validate src
-yarn validate
-yarn test
+# yarn validate
+# yarn test
