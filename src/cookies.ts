a7d217feeff5d12fe1b7fa07d58287ecaab8b926
George Haberis <george.haberis@guardian.co.uk>
2019-08-27 16:52:11 +0100

    update interface of cookies.ts

--- src/cookies.ts
+++ src/cookies.ts
@@ -1,5 +1,9 @@
 import * as Cookies from 'js-cookie';
 
+const GU_COOKIE_NAME = 'guconsent';
+const IAB_COOKIE_NAME = 'euconsent';
+const COOKIE_MAX_AGE = 395; // 13 months
+
 const getShortDomain = (): string => {
     const domain = document.domain || '';
 
@@ -17,21 +21,51 @@ const getDomainAttribute = (): string => {
     return shortDomain === 'localhost' ? '' : shortDomain;
 };
 
-const addCookie = (name: string, value: string, daysToLive?: number): void => {
+const addCookie = (name: string, value: string | GuPurposeState): void => {
     const options: {
         domain: string;
-        expires?: number;
+        expires: number;
     } = {
         domain: getDomainAttribute(),
+        expires: COOKIE_MAX_AGE,
     };
-
-    if (daysToLive) {
-        options.expires = daysToLive;
-    }
 
     Cookies.set(name, value, options);
 };
 
-const getCookie = (name: string): string | void => Cookies.get(name);
+const getCookie = (name: string): string | undefined => Cookies.get(name);
 
-export { addCookie, getCookie };
+const readGuCookie = (): GuPurposeState | null => {
+    const cookie = getCookie(GU_COOKIE_NAME);
+
+    if (!cookie) {
+        return null;
+    }
+
+    return JSON.parse(cookie);
+};
+
+const readIabCookie = (): string | null => {
+    const cookie = getCookie(IAB_COOKIE_NAME);
+
+    if (!cookie) {
+        return null;
+    }
+
+    return cookie;
+};
+
+const writeGuCookie = (guState: GuPurposeState): void =>
+    addCookie(GU_COOKIE_NAME, guState);
+
+const writeIabCookie = (iabString: string): void =>
+    addCookie(IAB_COOKIE_NAME, iabString);
+
+export { readGuCookie, readIabCookie, writeGuCookie, writeIabCookie };
+
+// exposed for testing purpose
+export const _ = {
+    GU_COOKIE_NAME,
+    IAB_COOKIE_NAME,
+    COOKIE_MAX_AGE,
+};
