3902000a95db2d3ffac15b97f75d0339054c7c0b
Ricardo Costa <ricardo.costa@guardian.co.uk>
2019-08-29 12:29:46 +0100

    Migrate constants

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,14 @@
 export const CMP_DOMAIN = 'https://manage.theguardian.com';
-export const CMP_SAVED_MSG = 'savedCmp';
+export const COOKIE_MAX_AGE = 395; // 13 months
 export const GU_AD_CONSENT_COOKIE = 'GU_TK';
 export const GU_COOKIE_NAME = 'guconsent';
+export const IAB_VENDOR_LIST_URL =
+    'https://assets.guim.co.uk/data/vendor/4f4a6324c7fe376c17ceb2288a84a076/cmp_vendorlist.json';
 export const IAB_COOKIE_NAME = 'euconsent';
-export const COOKIE_MAX_AGE = 395; // 13 months
+export const IAB_CMP_ID = 112;
+export const IAB_CMP_VERSION = 1;
+export const IAB_CONSENT_SCREEN = 0;
+export const IAB_CONSENT_LANGUAGE = 'en';
+export const CMP_READY_MSG = 'readyCmp';
+export const CMP_SAVED_MSG = 'savedCmp';
+export const CMP_CLOSE_MSG = 'closeCmp';
