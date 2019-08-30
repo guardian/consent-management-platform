9586ae995a72a17767edf29cbc811ee72b916c40
Ricardo Costa <ricardo.costa@guardian.co.uk>
2019-08-30 14:40:28 +0100

    Expose all variables and consts through index.ts

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import {
+    CMP_DOMAIN,
+    COOKIE_MAX_AGE,
+    GU_AD_CONSENT_COOKIE,
+    GU_COOKIE_NAME,
+    IAB_VENDOR_LIST_URL,
+    IAB_COOKIE_NAME,
+    IAB_CMP_ID,
+    IAB_CMP_VERSION,
+    IAB_CONSENT_SCREEN,
+    IAB_CONSENT_LANGUAGE,
+    CMP_READY_MSG,
+    CMP_SAVED_MSG,
+    CMP_CLOSE_MSG,
+    GU_PURPOSE_LIST,
+} from './config';
+import * as cookie from './cookies';
+
+export * from './cmp';
+export const cmpCookie = cookie;
+
+export const cmpConfig = {
+    CMP_DOMAIN,
+    COOKIE_MAX_AGE,
+    GU_AD_CONSENT_COOKIE,
+    GU_COOKIE_NAME,
+    IAB_VENDOR_LIST_URL,
+    IAB_COOKIE_NAME,
+    IAB_CMP_ID,
+    IAB_CMP_VERSION,
+    IAB_CONSENT_SCREEN,
+    IAB_CONSENT_LANGUAGE,
+    CMP_READY_MSG,
+    CMP_SAVED_MSG,
+    CMP_CLOSE_MSG,
+    GU_PURPOSE_LIST,
+};
