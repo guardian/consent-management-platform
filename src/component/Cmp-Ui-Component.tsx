72e87580eee8f0aabb1a5c97ec5e575cb4a66968
George Haberis <george.haberis@guardian.co.uk>
2019-10-21 16:31:56 +0100

    export cmp component

---
+++ src/component/Cmp-Ui-Component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { css } from '@emotion/core';
+
+const CMP = (): React.FC => (
+    <div
+        css={css`
+            color: red;
+            border: 2px solid blue;
+        `}
+    >
+        HELLO WORLD
+    </div>
+);
+
+export { CMP };
