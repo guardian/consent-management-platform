c796ff26e2c5fc8e26b5519b6ca96be8f6b3441b
George Haberis <george.haberis@guardian.co.uk>
2019-10-23 11:22:31 +0100

    add overlay

--- src/component/Cmp-Ui-Component.tsx
+++ src/component/Cmp-Ui-Component.tsx
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { css } from '@emotion/core';
+import { mobileLandscape } from '@guardian/src-foundations';
 
-const CMP: React.FC = () => (
-    <div
-        css={css`
-            color: red;
-            border: 2px solid blue;
-        `}
-    >
-        HELLO WORLD
-    </div>
-);
+const overlayStyles = css`
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    padding: 0;
+    margin: 0;
+    z-index: 9999;
+    background-color: transparent;
+    ${mobileLandscape} {
+        transition: background-color;
+        transition-delay: 0.6s;
+        will-change: background-color;
+    }
+`;
+
+const showOverlayStyles = css`
+    ${mobileLandscape} {
+        background-color: rgba(0, 0, 0, 0.7);
+    }
+`;
+
+interface State {
+    cmpVisible: boolean;
+}
+
+class CMP extends Component<{}, State> {
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            cmpVisible: true,
+        };
+    }
+
+    public render(): React.ReactNode {
+        const { cmpVisible } = this.state;
+
+        return (
+            <div
+                css={css`
+                    ${overlayStyles};
+                    ${cmpVisible ? showOverlayStyles : ''};
+                `}
+            ></div>
+        );
+    }
+}
 
 export { CMP };
