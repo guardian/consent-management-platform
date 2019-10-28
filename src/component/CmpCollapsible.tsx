69271f2f5f39589f8903e42ae0e14944deb7db23
George Haberis <george.haberis@guardian.co.uk>
2019-10-28 15:44:38 +0000

    add vendor and features lists

---
+++ src/component/CmpCollapsible.tsx
@@ -0,0 +1,98 @@
+import { css } from '@emotion/core';
+import { palette } from '@guardian/src-foundations';
+import React, { Component } from 'react';
+import { ItemState } from '../types';
+import { CollapseItemButton } from './CollapseItemButton';
+import { OnOffRadio } from './OnOffRadio';
+
+const titleTabStyles = css`
+    display: flex;
+    cursor: pointer;
+    margin-bottom: 18px;
+`;
+
+const titleContainerStyles = css`
+    flex: 1;
+`;
+
+const titleStyles = (collapsed: boolean) => css`
+    font-family: 'Guardian Text Sans Web', Helvetica Neue, Helvetica, Arial,
+        Lucida Grande, sans-serif;
+    font-size: 20px;
+    line-height: 22px;
+    font-weight: 700;
+    color: ${collapsed ? palette.yellow.dark : palette.neutral[100]};
+`;
+
+const panelStyles = (collapsed: boolean) => css`
+    display: ${collapsed ? 'block' : 'none'};
+    padding-left: 20px;
+
+    p {
+        font-family: 'Guardian Text Sans Web', Helvetica Neue, Helvetica, Arial,
+            Lucida Grande, sans-serif;
+        fontsize: 17px;
+        lineheight: 24px;
+    }
+`;
+
+interface Props {
+    title: string;
+    value?: ItemState;
+    updateItem?: (updatedValue: boolean) => void;
+    showError?: boolean;
+}
+
+interface State {
+    collapsed: boolean;
+}
+
+export class CmpCollapsible extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            collapsed: false,
+        };
+    }
+
+    public render(): React.ReactNode {
+        const { collapsed } = this.state;
+        const { title, value, children, updateItem, showError } = this.props;
+
+        return (
+            <>
+                <div css={titleTabStyles}>
+                    <div
+                        css={css`
+                            flex-grow: 1;
+                            display: flex;
+                        `}
+                        onClick={() => {
+                            this.toggleCollapsed();
+                        }}
+                    >
+                        <CollapseItemButton collapsed={collapsed} />
+                        <div css={titleContainerStyles}>
+                            <div css={titleStyles(collapsed)}>{title}</div>
+                        </div>
+                    </div>
+                    {value !== undefined && (
+                        <OnOffRadio
+                            selectedValue={value}
+                            onChangeHandler={updateItem}
+                            showError={showError}
+                        />
+                    )}
+                </div>
+                <div css={panelStyles(collapsed)}>{children}</div>
+            </>
+        );
+    }
+
+    private toggleCollapsed(): void {
+        this.setState((state, props) => ({
+            collapsed: !state.collapsed,
+        }));
+    }
+}
---
+++ src/component/OnOffRadio.tsx
@@ -0,0 +1,187 @@
+import { css } from '@emotion/core';
+import { ItemState } from '../types';
+import {
+    focusHalo,
+    palette,
+    size,
+    space,
+    transitions,
+} from '@guardian/src-foundations';
+import React, { Component } from 'react';
+
+let idCounter: number = 0;
+
+const radioContainerStyles = css`
+    cursor: default;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    flex-shrink: 0;
+`;
+
+const errorStyles = css`
+    box-shadow: 0 0 0 5px ${palette.news.bright};
+    z-index: 9;
+`;
+
+const radioInputStyles = css`
+    color: ${palette.brand.pastel};
+    @supports (appearance: none) {
+        appearance: none;
+        outline: 0;
+        color: inherit;
+        box-sizing: border-box;
+        display: inline-block;
+        width: ${size.small}px;
+        height: ${size.small}px;
+        margin: 0 ${space[1]}px 0 0;
+        border: 2px solid currentColor;
+        border-radius: 50%;
+        position: relative;
+        transition: box-shadow ${transitions.short};
+        transition-delay: 0.08s;
+        :focus {
+            ${focusHalo};
+        }
+        :after {
+            background: currentColor;
+            position: absolute;
+            content: '';
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            border-radius: 50%;
+            transform: scale(0);
+            transform-origin: center;
+            transition: transform ${transitions.short};
+        }
+        :checked {
+            color: ${palette.neutral[100]};
+            &:after {
+                transform: scale(0.6);
+            }
+        }
+        :disabled {
+            color: ${palette.brand.pastel};
+            &:checked {
+                color: ${palette.brand.pastel};
+            }
+            border-color: ${palette.brand.pastel};
+        }
+        :not([disabled]) {
+            cursor: pointer;
+        }
+    }
+`;
+const radioLabelStyles = (disabled: boolean) => css`
+    margin-left: ${space[2]}px;
+    cursor: ${disabled ? 'default' : 'pointer'};
+    display: flex;
+    align-items: center;
+    &:last-of-type {
+        margin-bottom: 0;
+    }
+    height: fit-content;
+
+    :hover {
+        input:not([disabled]) {
+            border-color: ${palette.neutral[100]};
+        }
+    }
+`;
+const radioLabelTextStyles = (disabled: boolean) => css`
+    position: relative;
+    font-family: 'Guardian Text Sans Web', Helvetica Neue, Helvetica, Arial,
+        Lucida Grande, sans-serif;
+    font-size: 17px;
+    color: ${disabled ? palette.brand.pastel : palette.neutral[100]};
+`;
+
+interface Props {
+    onChangeHandler?: (value: boolean) => void;
+    selectedValue?: ItemState;
+    showError?: boolean;
+}
+export class OnOffRadio extends Component<Props, {}> {
+    private myIdCounter: number;
+
+    constructor(props: Props) {
+        super(props);
+        this.myIdCounter = ++idCounter;
+    }
+
+    public render(): React.ReactNode {
+        const { selectedValue, onChangeHandler, showError } = this.props;
+        const id = `radio-${this.myIdCounter}`;
+        const onId = `${id}-on`;
+        const offId = `${id}-off`;
+        const disabled: boolean = !onChangeHandler;
+        const validationProps = showError
+            ? {
+                  'aria-invalid': true,
+              }
+            : {};
+
+        return (
+            <div css={radioContainerStyles}>
+                <label css={radioLabelStyles(disabled)}>
+                    <input
+                        id={offId}
+                        name={id}
+                        type="radio"
+                        value="off"
+                        onChange={(
+                            evt: React.ChangeEvent<HTMLInputElement>,
+                        ) => {
+                            this.updateValue(evt);
+                        }}
+                        defaultChecked={selectedValue === false}
+                        css={css`
+                            ${radioInputStyles};
+                            ${showError ? errorStyles : ''};
+                        `}
+                        disabled={disabled}
+                        {...validationProps}
+                    />
+                    <span css={radioLabelTextStyles(disabled)}>Off</span>
+                </label>
+                <label css={radioLabelStyles(disabled)}>
+                    <input
+                        id={onId}
+                        name={id}
+                        type="radio"
+                        value="on"
+                        onChange={(
+                            evt: React.ChangeEvent<HTMLInputElement>,
+                        ) => {
+                            this.updateValue(evt);
+                        }}
+                        defaultChecked={selectedValue === true}
+                        css={css`
+                            ${radioInputStyles};
+                            ${showError ? errorStyles : ''};
+                        `}
+                        disabled={disabled}
+                        {...validationProps}
+                    />
+                    <span css={radioLabelTextStyles(disabled)}>On</span>
+                </label>
+            </div>
+        );
+
+        return '';
+    }
+
+    public shouldComponentUpdate = (nextProps: Props) =>
+        nextProps.selectedValue === null || !!this.props.showError;
+
+    private updateValue(evt: React.ChangeEvent<HTMLInputElement>): void {
+        const value: boolean = evt.currentTarget.value === 'on';
+        const { onChangeHandler } = this.props;
+
+        if (onChangeHandler) {
+            onChangeHandler(value);
+        }
+    }
+}
