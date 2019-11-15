644b63d0122adfe46e79fdd7bfda80a5dfec651f
Ricardo Costa <ricardo.costa@guardian.co.uk>
2019-11-15 16:36:08 +0000

    Expanded error.ts unit tests

--- src/error.test.ts
+++ src/error.test.ts
@@ -1,14 +1,30 @@
 import { setErrorHandler, handleError } from './error';
 
-describe('Error', () => {
-    it('run the error handler correctly after one has been set ', () => {
-        const myCallback = jest.fn();
-        const errorMsg = 'An error happened';
+const myCallback = jest.fn();
+const errorMsg = 'An error happened';
 
+describe('Error', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('runs the error handler correctly after one has been set', () => {
         setErrorHandler(myCallback);
         handleError(errorMsg);
 
         expect(myCallback).toHaveBeenCalledTimes(1);
         expect(myCallback).toHaveBeenCalledWith(errorMsg);
+    });
+
+    it('runs the error handler everytime handleError is called', () => {
+        setErrorHandler(myCallback);
+
+        handleError(errorMsg);
+        handleError(errorMsg);
+        handleError(errorMsg);
+        handleError(errorMsg);
+        handleError(errorMsg);
+
+        expect(myCallback).toHaveBeenCalledTimes(5);
     });
 });
