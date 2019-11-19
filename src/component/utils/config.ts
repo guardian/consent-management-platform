f9473ad7f65860424114a97869d7033bde781fa6
George Haberis <george.haberis@guardian.co.uk>
2019-11-19 11:37:51 +0000

    use latest font-family names as defaults

--- src/component/utils/config.ts
+++ src/component/utils/config.ts
@@ -1,13 +1,11 @@
+import { FontsContextInterface } from '../../types';
+
 export const SCROLLABLE_ID = 'cmpScrollable';
 export const CONTENT_ID = 'cmpContent';
 export const PURPOSES_ID = 'cmpPurposes';
-export const DEFAULT_FONT_FAMILIES: {
-    headlineSerif: string;
-    bodySerif: string;
-    bodySans: string;
-} = {
-    headlineSerif: "'Guardian Egyptian Web', Georgia, serif",
-    bodySerif: "'Guardian Text Egyptian Web', Georgia, serif",
+export const DEFAULT_FONT_FAMILIES: FontsContextInterface = {
+    headlineSerif: 'GH Guardian Headline, Georgia, serif',
+    bodySerif: 'GuardianTextEgyptian, Georgia, serif',
     bodySans:
-        "'Guardian Text Sans Web', Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif",
+        'GuardianTextSans, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif',
 };
