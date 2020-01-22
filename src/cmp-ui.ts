bbc7b2e091a840b7483da2ebddfdf128dee42f4a
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-01-22 13:57:47 +0000

    ShouldShow now differentiates between re-permissioning conditions and non re-permissioning conditions

--- src/cmp-ui.ts
+++ src/cmp-ui.ts
@@ -1,3 +1,9 @@
-import { readIabCookie } from './cookies';
+import { readIabCookie, readLegacyCookie } from './cookies';
 
-export const shouldShow = (): boolean => !readIabCookie(); // TODO: Restore readGuCookie check once we start saving GU cookie
+export const shouldShow = (shouldRepermission = false): boolean => {
+    if (shouldRepermission) {
+        return !readIabCookie(); // TODO: Restore readGuCookie check once we start saving GU cookie
+    }
+
+    return !readIabCookie() && !readLegacyCookie();
+};
