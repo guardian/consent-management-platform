c9ccbafc9cf248b2f23dccf3ec9b2a236dc9b3ac
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-04-23 14:53:52 +0100

    remove unused config variables

--- src/component/utils/config.ts
+++ src/component/utils/config.ts
@@ -1,9 +1,6 @@
 import { FontsContextInterface } from '../../types';
 
 export const VENDORS_ID = 'cmp-vendors';
-export const SCROLLABLE_ID = 'cmpScrollable';
-export const CONTENT_ID = 'cmpContent';
-export const PURPOSES_ID = 'cmpPurposes';
 export const DEFAULT_FONT_FAMILIES: FontsContextInterface = {
     headlineSerif: 'GH Guardian Headline, Georgia, serif',
     bodySerif: 'GuardianTextEgyptian, Georgia, serif',
