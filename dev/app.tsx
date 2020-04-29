2fa6585d221f42ec33e8d8c12e8e42a26e32bc0f
Alex Sanders <a@snd.rs>
2020-04-29 16:47:19 +0100

    do something

--- dev/app.tsx
+++ dev/app.tsx
@@ -11,8 +11,12 @@ import { ConsentManagementPlatform } from '../src/component/ConsentManagementPla
 //     onIabConsentNotification,
 // } from '../src';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// eslint-disable-next-line no-console
-const onClose = () => console.log('closed it');
+const onClose = () => {
+    // do something with:
+    //     onGuConsentNotification,
+    //     setErrorHandler,
+    //     shouldShow,
+    //     onIabConsentNotification,
+};
 
 ReactDOM.render(<ConsentManagementPlatform onClose={onClose} />, document.body);
