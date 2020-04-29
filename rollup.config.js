e9062c3ccd3e2363a774ecafbf099b802a40963b
Alex Sanders <a@snd.rs>
2020-04-29 16:57:02 +0100

    restore prod config to original
    it’s nothing to do with dev server

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,24 @@ import resolve from 'rollup-plugin-node-resolve';
 
 const extensions = ['.ts', '.tsx'];
 
-// eslint-disable-next-line import/no-default-export
-export default () => ({
-    input: {
-        index: 'src/index.ts',
-        ConsentManagementPlatform:
-            'src/component/ConsentManagementPlatform.tsx',
-    },
-    output: {
-        dir: 'lib',
-        format: 'cjs',
-    },
+module.exports = {
+    input: ['src/index.ts', 'src/component/ConsentManagementPlatform.tsx'],
+    output: [
+        {
+            dir: 'lib',
+            format: 'cjs',
+        },
+        {
+            dir: 'lib',
+            format: 'cjs',
+        },
+    ],
     external: [
         'react',
         '@emotion/core',
         '@guardian/src-button',
         '@guardian/src-foundations',
         '@guardian/src-svgs',
-        '@guardian/src-foundations/mq',
-        '@guardian/src-foundations/typography',
-        '@guardian/src-foundations/accessibility',
-        '@babel/runtime/helpers/extends',
-        '@babel/runtime/helpers/defineProperty',
-        '@guardian/src-foundations/palette',
-        '@guardian/src-foundations/themes',
     ],
     plugins: [babel({ extensions }), resolve({ extensions }), commonjs()],
-});
+};
