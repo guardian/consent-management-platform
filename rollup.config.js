cf47be11be8b3c36e6d59fb3d36ed89c45a8638a
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-05-04 14:00:00 +0100

    Remove component build steps

--- rollup.config.js
+++ rollup.config.js
@@ -2,26 +2,15 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
-const extensions = ['.ts', '.tsx'];
+const extensions = ['.ts'];
 
 module.exports = {
-    input: ['src/index.ts', 'src/component/ConsentManagementPlatform.tsx'],
+    input: ['src/index.ts'],
     output: [
         {
             dir: 'lib',
             format: 'cjs',
         },
-        {
-            dir: 'lib',
-            format: 'cjs',
-        },
-    ],
-    external: [
-        'react',
-        '@emotion/core',
-        '@guardian/src-button',
-        '@guardian/src-foundations',
-        '@guardian/src-svgs',
     ],
     plugins: [babel({ extensions }), resolve({ extensions }), commonjs()],
 };
