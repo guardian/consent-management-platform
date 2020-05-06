61c57441f86341092c3be1b10c08fc4744455915
Alex Sanders <a@snd.rs>
2020-05-06 13:34:44 +0100

    create gh-pages build

---
+++ rollup.config.gh-pages.js
@@ -0,0 +1,28 @@
+import html from '@rollup/plugin-html';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
+import path from 'path';
+
+const extensions = ['.js', '.ts'];
+
+const dist = '.gh-pages';
+
+export default {
+    input: path.resolve(__dirname, 'dev', 'app.js'),
+    output: {
+        format: 'esm',
+        dir: dist,
+        sourcemap: 'inline',
+    },
+    plugins: [
+        babel({ extensions }),
+        resolve({ extensions }),
+        commonjs(),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        html(),
+    ],
+};
