59308f01f7bb818c422f48aa2ab529d9573935c3
Alex Sanders <a@snd.rs>
2020-05-27 18:26:33 +0100

    terser

--- src/sourcepoint.test.ts
+++ src/sourcepoint.test.ts
@@ -1,24 +1,26 @@
-import { init } from './sourcepoint'
-import http from 'http'
-import url from 'url'
+import { init } from './sourcepoint';
+import http from 'http';
+import url from 'url';
 
 describe('Sourcepoint', () => {
     init();
 
     it('injects the stub', () => {
         expect(document.getElementById('sourcepoint-ccpa-stub')).toBeTruthy();
-    })
+    });
 
     it('injects the lib', () => {
-        const lib = document.getElementById('sourcepoint-ccpa-lib');
-        expect(lib).toBeTruthy();
-    })
+        expect(document.getElementById('sourcepoint-ccpa-lib')).toBeTruthy();
+    });
 
-    it('points at a real file', (done) => {
-        const lib = document.getElementById('sourcepoint-ccpa-lib');
-        const src = url.parse(lib.getAttribute('src'));
-        const req = http.request({ method: 'HEAD', host: src.host, port: 80, path: src.path }, () => done());
-        req.end()
-    })
-
-})
+    it('points at a real file', done => {
+        const src = url.parse(
+            document.getElementById('sourcepoint-ccpa-lib').getAttribute('src'),
+        );
+        const req = http.request(
+            { method: 'HEAD', host: src.host, port: 80, path: src.path },
+            () => done(),
+        );
+        req.end();
+    });
+});
