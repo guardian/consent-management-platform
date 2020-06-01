26acf3de14da048c298099d11e1c2a8fc3b2c1a2
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-06-01 12:54:18 +0100

    Add comment for clarity

--- src/ccpa/core.ts
+++ src/ccpa/core.ts
@@ -3,5 +3,7 @@ export type CcpaPurposeCallback = (state: boolean) => void;
 export const onIabConsentNotification = (
     callback: CcpaPurposeCallback,
 ): void => {
+    // Leaving this console.log as a placeholder. Will be removed in next PR.
+    // eslint-disable-next-line no-console
     console.log('Received a callback function named', callback.name);
 };
