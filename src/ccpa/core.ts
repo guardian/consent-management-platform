f29d77aafe8b67134d9873e897ec8fc7b79c46bf
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-06-01 12:40:50 +0100

    Add CCPA event listeners

--- src/ccpa/core.ts
+++ src/ccpa/core.ts
@@ -1,9 +1,52 @@
+import { init as initSourcepoint } from './sourcepoint';
+
 export type CcpaPurposeCallback = (state: boolean) => void;
+
+const ccpaCallbackList: CcpaPurposeCallback[] = [];
+
+let initialised = false;
+let ccpaState = false;
+
+export const init = () => {
+    initSourcepoint(runCallbacksOnCcpaReady);
+    updateCcpaStateAnd(() => {
+        initialised = true;
+    });
+};
 
 export const onIabConsentNotification = (
     callback: CcpaPurposeCallback,
 ): void => {
-    // Leaving this console.log as a placeholder. Will be removed in next PR.
-    // eslint-disable-next-line no-console
-    console.log('Received a callback function named', callback.name);
+    if (initialised) {
+        callback(ccpaState);
+    }
+    ccpaCallbackList.push(callback);
+};
+
+const runCallbacksOnCcpaReady = () => {
+    updateCcpaStateAnd(runCallbacks);
+};
+
+const runCallbacks = () => {
+    ccpaCallbackList.forEach(cb => cb(ccpaState));
+};
+
+const updateCcpaStateAnd = (doThis: () => void) => {
+    // eslint-disable-next-line no-underscore-dangle
+    window.__uspapi('getUSPData', 1, (uspData, success) => {
+        console.log('getUSPData:', success, uspData);
+        if (
+            success &&
+            uspData?.uspString?.charAt(2) === 'Y'
+            // uspData &&
+            // uspData.uspString &&
+            // uspData.uspString.charAt(2) === 'Y'
+        ) {
+            ccpaState = true;
+        } else {
+            ccpaState = false;
+        }
+
+        doThis();
+    });
 };
