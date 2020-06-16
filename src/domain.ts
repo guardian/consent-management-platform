431d6b19b7348efaa8d2c12324d306e96a657e4b
Alex Sanders <a@snd.rs>
2020-06-16 15:51:51 +0100

    check full guardian domain

---
+++ src/domain.ts
@@ -0,0 +1,7 @@
+let isGuardian: boolean | undefined;
+
+export const isGuardianDomain = () => {
+    if (typeof isGuardian === 'undefined')
+        isGuardian = /\.theguardian\.com$/.test(window.location.host);
+    return isGuardian;
+};
