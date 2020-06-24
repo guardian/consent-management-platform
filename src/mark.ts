90881bc87e23c3c4f074b7132c150baa6fb2b662
Alex Sanders <a@snd.rs>
2020-06-24 15:34:11 +0100

    strip debug from production build

--- src/mark.ts
+++ src/mark.ts
@@ -1,8 +1,6 @@
-import { isGuardianDomain } from './domain';
-
 export const mark = (label: string) => {
 	window.performance?.mark?.(label);
-	if (!isGuardianDomain() && process.env.NODE_ENV !== 'test') {
+	if (process.env.NODE_ENV !== 'test') {
 		// eslint-disable-next-line no-console
 		console.log(label);
 	}
