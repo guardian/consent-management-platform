340968e918b558d8c47f3f8fd617b20abc3eb2dc
Josh Buckland <buck06191@gmail.com>
2020-06-30 16:25:18 +0100

    Stop CCPA stub being added to every page

--- src/ccpa/sourcepoint.ts
+++ src/ccpa/sourcepoint.ts
@@ -32,6 +32,13 @@ declare global {
 	}
 }
 
+const createCcpaStubScriptElement = (): HTMLElement => {
+	const ccpaStubSrc = `(function () { var e = false; var c = window; var t = document; function r() { if (!c.frames["__uspapiLocator"]) { if (t.body) { var a = t.body; var e = t.createElement("iframe"); e.style.cssText = "display:none"; e.name = "__uspapiLocator"; a.appendChild(e) } else { setTimeout(r, 5) } } } r(); function p() { var a = arguments; __uspapi.a = __uspapi.a || []; if (!a.length) { return __uspapi.a } else if (a[0] === "ping") { a[2]({ gdprAppliesGlobally: e, cmpLoaded: false }, true) } else { __uspapi.a.push([].slice.apply(a)) } } function l(t) { var r = typeof t.data === "string"; try { var a = r ? JSON.parse(t.data) : t.data; if (a.__cmpCall) { var n = a.__cmpCall; c.__uspapi(n.command, n.parameter, function (a, e) { var c = { __cmpReturn: { returnValue: a, success: e, callId: n.callId } }; t.source.postMessage(r ? JSON.stringify(c) : c, "*") }) } } catch (a) { } } if (typeof __uspapi !== "function") { c.__uspapi = p; __uspapi.msgHandler = l; c.addEventListener("message", l, false) } })();`;
+	const ccpaStub = document.createElement('script');
+	ccpaStub.id = 'sourcepoint-ccpa-stub';
+	ccpaStub.innerHTML = ccpaStubSrc;
+	return ccpaStub;
+};
 export interface MsgData {
 	msg_id: number;
 }
@@ -68,6 +75,8 @@ export const init = (onCcpaReadyCallback: onReadyCallback) => {
 			'Sourcepoint CCPA global (window._sp_ccpa) is already defined!',
 		);
 	}
+	const ccpaStub = createCcpaStubScriptElement();
+	document.head.appendChild(ccpaStub);
 
 	window._sp_ccpa = {
 		config: {
