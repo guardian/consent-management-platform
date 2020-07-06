2d8b3a51f33c8e1b01bd463c6c6754c7206c6cac
Alex Sanders <a@snd.rs>
2020-07-06 13:20:13 +0100

    rejig sp ccpa

--- src/ccpa/sourcepoint.ts
+++ src/ccpa/sourcepoint.ts
@@ -1,65 +1,18 @@
 /* eslint-disable no-underscore-dangle */
 
-import { runStub } from './sourcepoint-stub';
-import { mark } from '../mark';
-import { isGuardianDomain } from '../domain';
+import { stub } from './stub';
+import { mark } from '../lib/mark';
+import { isGuardianDomain } from '../lib/domain';
+import { ACCOUNT_ID } from '../lib/accountId';
+import { invokeCallbacks } from '../onConsent';
 
-// https://documentation.sourcepoint.com/web-implementation/sourcepoint-gdpr-and-tcf-v2-support-beta/gdpr-and-tcf-v2-setup-and-configuration#1-two-step-process-to-implement-the-gdpr-and-tcf-v2-code-snippet
+let resolveWillShowPrivacyMessage: Function | undefined;
+export const willShowPrivacyMessage = new Promise<boolean>((resolve) => {
+	resolveWillShowPrivacyMessage = resolve;
+});
 
-declare global {
-	interface Window {
-		_sp_ccpa: {
-			config: {
-				mmsDomain: 'https://consent.theguardian.com';
-				ccpaOrigin: 'https://ccpa-service.sp-prod.net';
-				accountId: string;
-				getDnsMsgMms: boolean;
-				alwaysDisplayDns: boolean;
-				siteHref: string | null;
-				events?: {
-					onConsentReady?: () => void;
-					onMessageReady?: () => void;
-					onMessageReceiveData?: onMessaReceiveDataCallback;
-				};
-			};
-			loadPrivacyManagerModal?: (unknown: unknown, id: string) => {}; // this function is undocumented
-		};
-		__uspapi: (
-			command: string,
-			version: number,
-			callback: (uspdata: UspData | undefined, success: boolean) => void,
-		) => void;
-	}
-}
-
-export interface MsgData {
-	msg_id: number;
-}
-
-interface UspData {
-	version: number;
-	uspString: string;
-}
-
-type onReadyCallback = () => void;
-type onMessaReceiveDataCallback = (data: MsgData) => void;
-type PromiseResolver = (result: boolean) => void;
-
-const accountId = '1257';
-
-const ccpaLib = document.createElement('script');
-ccpaLib.id = 'sourcepoint-ccpa-lib';
-ccpaLib.src = 'https://ccpa.sp-prod.net/ccpa.js';
-
-let willShowUi: Promise<boolean> | null = null;
-let willShowUiResolver: PromiseResolver | null = null;
-
-export const init = (onCcpaReadyCallback: onReadyCallback) => {
-	mark('cmp-ccpa-init');
-
-	willShowUi = new Promise<boolean>((resolve) => {
-		willShowUiResolver = resolve;
-	});
+export const init = () => {
+	stub();
 
 	// make sure nothing else on the page has accidentally
 	// used the _sp_* name as well
@@ -68,34 +21,33 @@ export const init = (onCcpaReadyCallback: onReadyCallback) => {
 			'Sourcepoint CCPA global (window._sp_ccpa) is already defined!',
 		);
 	}
-	runStub();
 
 	window._sp_ccpa = {
 		config: {
 			mmsDomain: 'https://consent.theguardian.com',
 			ccpaOrigin: 'https://ccpa-service.sp-prod.net',
-			accountId,
+			accountId: ACCOUNT_ID,
 			getDnsMsgMms: true,
 			alwaysDisplayDns: false,
 			siteHref: isGuardianDomain() ? null : 'https://test.theguardian.com',
+
 			events: {
 				onConsentReady() {
 					mark('cmp-ccpa-got-consent');
-					onCcpaReadyCallback();
+					invokeCallbacks();
 				},
 				onMessageReady: () => {
 					mark('cmp-ccpa-ui-displayed');
 				},
 				onMessageReceiveData: (data) => {
-					willShowUiResolver?.(data.msg_id !== 0);
+					resolveWillShowPrivacyMessage?.(data.msg_id !== 0);
 				},
 			},
 		},
 	};
 
+	const ccpaLib = document.createElement('script');
+	ccpaLib.id = 'sourcepoint-ccpa-lib';
+	ccpaLib.src = 'https://ccpa.sp-prod.net/ccpa.js';
 	document.body.appendChild(ccpaLib);
-};
-
-export const checkWillShowUi = (): Promise<boolean> => {
-	return willShowUi ?? Promise.reject();
 };
