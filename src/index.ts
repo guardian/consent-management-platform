d00bd9a9d36aab5d07ee75498b5d9850cfbd1de0
Alex Sanders <a@snd.rs>
2020-07-07 14:32:41 +0100

    remove old part of CMP (missed a bit)

--- src/index.ts
+++ src/index.ts
@@ -40,13 +40,3 @@ export const cmp = {
 	showPrivacyManager,
 	onConsent,
 };
-
-// // race condition - called before init so ccpa is false
-// export const onIabConsentNotification = (callback: IabPurposeCallback) =>
-// 	CCPA_APPLIES
-// 		? ccpaOnIabConsentNotification(callback as CcpaPurposeCallback)
-// 		: tcfOnIabConsentNotification(callback as TcfPurposeCallback);
-//
-// export { setErrorHandler } from './tcf/error';
-// export { shouldShow } from './tcf/cmp-ui';
-// export { onGuConsentNotification } from './tcf/core';
