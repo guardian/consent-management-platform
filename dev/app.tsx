d169a4aa6dd45003f29c0bf26e9310a6580cf48a
Alex Sanders <a@snd.rs>
2020-07-07 14:29:46 +0100

    add staging sned reset button to dev page

--- dev/app.tsx
+++ dev/app.tsx
@@ -27,10 +27,41 @@ locationControl.onclick = () => {
 };
 
 locationLabel.appendChild(locationControl);
-
 document.body.append(locationLabel);
+
+const stagingLabel = document.createElement('label');
+stagingLabel.innerHTML = 'use staging campaign';
+stagingLabel.style.display = 'flex';
+stagingLabel.style.flexDirection = 'row-reverse';
+stagingLabel.style.justifyContent = 'flex-end';
+
+const stagingControl = document.createElement('input');
+stagingControl.type = 'checkbox';
+stagingControl.checked = window.location.search === '?_sp_env=stage';
+stagingControl.onclick = () => {
+	window.location.search = stagingControl.checked ? '_sp_env=stage' : '';
+};
+
+stagingLabel.appendChild(stagingControl);
+document.body.append(stagingLabel);
+
 const settingsButton = document.createElement('button');
 settingsButton.innerText = 'show privacy settings';
 settingsButton.onclick = cmp.showPrivacyManager;
 settingsButton.style.marginTop = '1rem';
+settingsButton.style.display = 'block';
 document.body.append(settingsButton);
+
+const resetButton = document.createElement('button');
+resetButton.innerText = 'clear settings';
+resetButton.onclick = () => {
+	document.cookie.split(';').forEach((cookie) => {
+		document.cookie = cookie
+			.replace(/^ +/, '')
+			.replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`);
+	});
+	window.location.reload();
+};
+resetButton.style.marginTop = '1rem';
+settingsButton.style.display = 'block';
+document.body.append(resetButton);
