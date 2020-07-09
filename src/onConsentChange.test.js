738268056260e76af4f38fa8140ad7c1b4b3c9f4
Alex Sanders <a@snd.rs>
2020-07-09 11:34:46 +0100

    TCFv2 index tests (#161)
    * tests for entry file
    
    * these don’t really need tests
    
    * move comment back
    
    * remove for now
    
    * add test coverage reporting
    
    * how about this one?

--- src/onConsentChange.test.js
+++
@@ -1,6 +0,0 @@
-// describe('onConsentChange', () => {
-// 	it('invokes callbacks regardless of when the cmp is initialised', () => {});
-// 	it('invokes a new callback immediately if consent state is known', () => {});
-// 	it('only invokes callbacks if the consent state has changed', () => {});
-// 	it('invokes callbacks with the relevant privacy framework', () => {});
-// });
