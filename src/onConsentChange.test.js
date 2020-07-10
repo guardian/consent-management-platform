0aa5b926902c2b739086e113861df7192156ca45
Max Duval <max@mxdvl.com>
2020-07-10 12:20:22 +0100

    added onConsentState tests

--- src/onConsentChange.test.js
+++ src/onConsentChange.test.js
@@ -1,5 +1,59 @@
+/* eslint-disable no-underscore-dangle */
+import waitForExpect from 'wait-for-expect';
+import { onConsentChange, invokeCallbacks, _ } from './onConsentChange';
+
+const uspData = {
+	version: 1,
+	uspString: '1YYN',
+};
+window.__uspapi = jest.fn((a, b, callback) => {
+	callback(uspData, true);
+});
+
 describe('onConsentChange', () => {
-	it('has a test', () => {
-		expect(true).toBeTruthy();
+	it('can fake the USP Api', () => {
+		expect(window.__uspapi).toBeDefined();
+	});
+
+	it('invokes callbacks correctly', () => {
+		// const shouldChange = false;
+		const callback = jest.fn();
+		const instantCallback = jest.fn();
+		onConsentChange(callback);
+		expect(callback).toHaveBeenCalledTimes(0);
+		invokeCallbacks();
+		return waitForExpect(() => {
+			expect(callback).toHaveBeenCalledTimes(1);
+		}).then(() => {
+			onConsentChange(instantCallback);
+			return waitForExpect(() => {
+				expect(callback).toHaveBeenCalledTimes(1);
+				expect(instantCallback).toHaveBeenCalledTimes(1);
+			});
+		});
+	});
+
+	it('invokes callbacks only if there is a new state', () => {
+		const callback = jest.fn();
+		onConsentChange(callback);
+		invokeCallbacks();
+		return waitForExpect(() => {
+			expect(callback).toHaveBeenCalledTimes(1);
+		})
+			.then(invokeCallbacks)
+			.then(() => {
+				return waitForExpect(() => {
+					expect(callback).toHaveBeenCalledTimes(1);
+				});
+			})
+			.then(() => {
+				uspData.uspString = '1YNN';
+			})
+			.then(invokeCallbacks)
+			.then(() =>
+				waitForExpect(() => {
+					expect(callback).toHaveBeenCalledTimes(2);
+				}),
+			);
 	});
 });
