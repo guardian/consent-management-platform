9c0b81bd2052d92323af308ff2ef47b4df1fe7b4
Alex Sanders <a@snd.rs>
2020-09-01 17:30:49 +0100

    minify differetn configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,60 @@
 import strip from '@rollup/plugin-strip';
 import babel from 'rollup-plugin-babel';
+import bundleSize from 'rollup-plugin-bundle-size';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
-import bundleSize from 'rollup-plugin-bundle-size';
 import pkg from './package.json';
 
 const extensions = ['.js', '.ts', '.tsx'];
 
-module.exports = {
-	input: 'src/index.ts',
-	output: [
-		{
+const input = 'src/index.ts';
+
+const plugins = ({ modules = false } = {}) => [
+	babel({
+		extensions,
+		...(modules && {
+			presets: [
+				['@babel/preset-env', { targets: { esmodules: true }, bugfixes: true }],
+			],
+		}),
+	}),
+	resolve({ extensions }),
+	replace({
+		'process.env.NODE_ENV': JSON.stringify('production'),
+	}),
+	commonjs(),
+	strip({
+		include: ['**/*.{j,t}s?(x)'],
+		sourceMap: true,
+	}),
+	terser({ safari10: true }),
+	bundleSize(),
+];
+
+const external = [
+	'@guardian/old-cmp',
+	'@guardian/old-cmp/dist/ConsentManagementPlatform',
+];
+
+module.exports = [
+	{
+		input,
+		output: {
 			file: pkg.main,
 			format: 'cjs',
 		},
-		{
+		external,
+		plugins: plugins(),
+	},
+	{
+		input,
+		output: {
 			file: pkg.module,
 			format: 'esm',
 		},
-	],
-	external: [
-		'@guardian/old-cmp',
-		'@guardian/old-cmp/dist/ConsentManagementPlatform',
-	],
-	plugins: [
-		babel({ extensions }),
-		resolve({ extensions }),
-		replace({
-			'process.env.NODE_ENV': JSON.stringify('production'),
-		}),
-		commonjs(),
-		strip({
-			include: ['**/*.{j,t}s?(x)'],
-			sourceMap: true,
-		}),
-		terser({ safari10: true }),
-		bundleSize(),
-	],
-};
+		external,
+		plugins: plugins({ modules: true }),
+	},
+];
