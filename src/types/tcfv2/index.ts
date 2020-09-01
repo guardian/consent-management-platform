1e4ab31dec7cbb10018239689ede1c8b4c1669fe
Alex Sanders <a@snd.rs>
2020-09-01 12:04:53 +0100

    comment

--- src/types/tcfv2/index.ts
+++ src/types/tcfv2/index.ts
@@ -2,16 +2,17 @@ export interface TCFv2ConsentList {
 	[key: string]: boolean;
 }
 
-export type TCEventStatusCode =
-	| 'tcloaded'
-	| 'cmpuishown'
-	| 'useractioncomplete';
-
 export interface TCFv2ConsentState {
 	consents: TCFv2ConsentList;
 	eventStatus: TCEventStatusCode;
 	vendorConsents: TCFv2ConsentList;
 }
+
+// From the IAB spec – https://git.io/JUmoi
+export type TCEventStatusCode =
+	| 'tcloaded'
+	| 'cmpuishown'
+	| 'useractioncomplete';
 
 // From the IAB spec – https://git.io/JUmw8
 export type TCPingStatusCode =
