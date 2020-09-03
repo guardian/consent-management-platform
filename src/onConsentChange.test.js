70a50c88003113874184aefb9f1d5325beed4b78
Alex Sanders <a@snd.rs>
2020-09-03 12:56:56 +0100

    move things out of main file

--- src/onConsentChange.test.js
+++ src/onConsentChange.test.js
@@ -1,73 +1,70 @@
 /* eslint-disable no-underscore-dangle */
 import waitForExpect from 'wait-for-expect';
-import { _, invokeCallbacks, onConsentChange } from './onConsentChange';
+import { invokeCallbacks, onConsentChange } from './onConsentChange';
 
 const uspData = {
 	version: 1,
 	uspString: '1YYN',
 };
+
 window.__uspapi = jest.fn((a, b, callback) => {
 	callback(uspData, true);
 });
-const tcfData = {
-	purposes: {
-		consents: {
-			1: true,
-			2: false,
-			3: true,
-		},
-	},
-};
+
+// const tcfData = {
+// 	purposes: {
+// 		consents: {
+// 			1: true,
+// 			2: false,
+// 			3: true,
+// 		},
+// 	},
+// };
 
 describe('onConsentChange', () => {
-	it('invokes callbacks correctly', () => {
+	it('invokes callbacks correctly', async () => {
 		const callback = jest.fn();
 		const instantCallback = jest.fn();
+
 		onConsentChange(callback);
+
 		expect(callback).toHaveBeenCalledTimes(0);
+
 		invokeCallbacks();
-		return waitForExpect(() => {
+
+		await waitForExpect(() => {
 			expect(callback).toHaveBeenCalledTimes(1);
-		}).then(() => {
-			onConsentChange(instantCallback);
-			return waitForExpect(() => {
-				expect(callback).toHaveBeenCalledTimes(1);
-				expect(instantCallback).toHaveBeenCalledTimes(1);
-			});
+		});
+
+		onConsentChange(instantCallback);
+
+		await waitForExpect(() => {
+			expect(callback).toHaveBeenCalledTimes(1);
+			expect(instantCallback).toHaveBeenCalledTimes(1);
 		});
 	});
 
-	it('invokes callbacks only if there is a new state', () => {
+	it('invokes callbacks only if there is a new state', async () => {
 		const callback = jest.fn();
+
 		onConsentChange(callback);
 		invokeCallbacks();
-		return waitForExpect(() => {
+
+		await waitForExpect(() => {
 			expect(callback).toHaveBeenCalledTimes(1);
-		})
-			.then(invokeCallbacks)
-			.then(() => {
-				return waitForExpect(() => {
-					expect(callback).toHaveBeenCalledTimes(1);
-				});
-			})
-			.then(() => {
-				uspData.uspString = '1YNN';
-			})
-			.then(invokeCallbacks)
-			.then(() =>
-				waitForExpect(() => {
-					expect(callback).toHaveBeenCalledTimes(2);
-				}),
-			);
-	});
+		});
 
-	it('returns all 10 TCF purposes even if they are false', () => {
-		const consents = _.fillAllConsents(tcfData.purposes.consents);
+		invokeCallbacks();
 
-		expect(Object.keys(consents)).toHaveLength(10);
-		expect(consents[1]).toEqual(true);
-		expect(consents[3]).toEqual(true);
-		expect(consents[9]).toEqual(false);
-		expect(consents[10]).toBeDefined();
+		await waitForExpect(() => {
+			expect(callback).toHaveBeenCalledTimes(1);
+		});
+
+		uspData.uspString = '1YNN';
+		invokeCallbacks();
+
+		await waitForExpect(() => {
+			expect(callback).toHaveBeenCalledTimes(2);
+		});
 	});
 });
