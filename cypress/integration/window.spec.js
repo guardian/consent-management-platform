591c872fea02143123498fbc2cc50850ecdc816e
Max Duval <max@mxdvl.com>
2020-09-08 17:25:09 +0100

    update wording

--- cypress/integration/window.spec.js
+++ cypress/integration/window.spec.js
@@ -25,7 +25,7 @@ describe('Document', () => {
 			.should('have.id', 'sp_message_iframe_208529');
 	});
 
-	it('Is added to the page with the correct URL', () => {
+	it('should have the correct script URL', () => {
 		cy.get('script#sourcepoint-tcfv2-lib').should(
 			'have.attr',
 			'src',
