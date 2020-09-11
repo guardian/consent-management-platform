994f07c736072bf6386e6e535361ab2a7cbf8c6f
Max Duval <max@mxdvl.com>
2020-09-11 15:45:08 +0100

    clean cy support file

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,7 +16,3 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-// import '@cypress/skip-test/support';
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
