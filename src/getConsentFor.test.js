ee235485b23858e6fea8bb1f3f5d99320f5c31d7
Alex Sanders <a@snd.rs>
2020-09-16 11:21:15 +0100

    implement getConsentFor
    Co-authored-by: Alex Sanders <a@snd.rs>

---
+++ src/getConsentFor.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-underscore-dangle */
+import { getConsentFor } from './getConsentFor';
+
+const consentStateNotFound = {
+	tcfv2: { vendorConsents: { doesnotexist: true } },
+};
+
+const consentStateFoundTrue = {
+	tcfv2: { vendorConsents: { '5e542b3a4cd8884eb41b5a72': true } },
+};
+
+const consentStateFoundFalse = {
+	tcfv2: { vendorConsents: { '5e542b3a4cd8884eb41b5a72': false } },
+};
+
+it('throws an error if the vendor found ', () => {
+	expect(() => {
+		getConsentFor('doesnotexist', consentStateFoundTrue);
+	}).toThrow("Vendor 'doesnotexist' not found");
+});
+
+it('throws an error if no consent found for specific vendor ', () => {
+	expect(() => {
+		getConsentFor('google-analytics', consentStateNotFound);
+	}).toThrow("No consent returned from SP for vendor: 'google-analytics'");
+});
+
+it('returns true consent if true', () => {
+	expect(getConsentFor('google-analytics', consentStateFoundTrue)).toBeTruthy();
+});
+
+it('returns false consent if false', () => {
+	expect(getConsentFor('google-analytics', consentStateFoundFalse)).toBeFalsy();
+});
