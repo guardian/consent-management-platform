646f588cf9331116159d91fad82b67466b040eab
Max Duval <max@mxdvl.com>
2020-10-06 15:04:12 +0100

    revert 68f6ff24
    - Impossible to run TCFv2 in CI

--- cypress/integration/sourcepoint-tcfv2.spec.js
+++ cypress/integration/sourcepoint-tcfv2.spec.js
@@ -40,82 +40,91 @@ describe('Document', () => {
 	});
 });
 
-describe('Interaction', () => {
-	loadPage();
-	const buttonTitle = 'Yes, I’m happy';
+// TODO: enable testing of TCFv2 on CI
+skipOn(Cypress.env('CI') === 'true', () => {
+	describe('Interaction', () => {
+		loadPage();
+		const buttonTitle = 'Yes, I’m happy';
 
-	beforeEach(() => {
-		cy.setCookie('ccpaApplies', 'false');
-		Cypress.Cookies.preserveOnce('consentUUID', 'euconsent-v2');
-	});
-
-	it(`should give all consents when clicking "${buttonTitle}"`, () => {
-		cy.getIframeBody(iframeMessage)
-			.find(`button[title="${buttonTitle}"]`)
-			.click();
-
-		// eslint-disable-next-line cypress/no-unnecessary-waiting
-		cy.wait(1000);
-
-		[(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)].forEach((purpose) => {
-			cy.get(`[data-purpose="${purpose}"]`).should(
-				'have.data',
-				'consent',
-				true,
-			);
+		beforeEach(() => {
+			cy.setCookie('ccpaApplies', 'false');
+			cy.setCookie('gdprApplies', 'true');
+			Cypress.Cookies.preserveOnce('consentUUID', 'euconsent-v2');
 		});
-	});
-	it(`should be able to only deactivate purpose 1`, () => {
-		cy.get('[data-cy=pm]').click();
 
-		cy.getIframeBody(iframePrivacyManager)
-			.find(`label[aria-label="Store and/or access information on a device"]`)
-			.find('span.off')
-			.click();
+		it(`should give all consents when clicking "${buttonTitle}"`, () => {
+			cy.getIframeBody(iframeMessage)
+				.find(`button[title="${buttonTitle}"]`)
+				.click();
 
-		cy.getIframeBody(iframePrivacyManager)
-			.find(`button[aria-label="Save and close"]`)
-			.click();
+			// eslint-disable-next-line cypress/no-unnecessary-waiting
+			cy.wait(1000);
 
-		// eslint-disable-next-line cypress/no-unnecessary-waiting
-		cy.wait(1000);
+			[(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)].forEach((purpose) => {
+				cy.get(`[data-purpose="${purpose}"]`).should(
+					'have.data',
+					'consent',
+					true,
+				);
+			});
+		});
+		it(`should be able to only deactivate purpose 1`, () => {
+			cy.get('[data-cy=pm]').click();
 
-		cy.get(`[data-purpose="1"]`)
-			.should('have.data', 'consent')
-			.should('equal', false);
+			cy.getIframeBody(iframePrivacyManager)
+				.find(`label[aria-label="Store and/or access information on a device"]`)
+				.find('span.off')
+				.click();
 
-		[2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((purpose) => {
-			cy.get(`[data-purpose="${purpose}"]`)
+			cy.getIframeBody(iframePrivacyManager)
+				.find(`button[aria-label="Save and close"]`)
+				.click();
+
+			// eslint-disable-next-line cypress/no-unnecessary-waiting
+			cy.wait(1000);
+
+			cy.get(`[data-purpose="1"]`)
 				.should('have.data', 'consent')
-				.should('equal', true);
+				.should('equal', false);
+
+			[2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((purpose) => {
+				cy.get(`[data-purpose="${purpose}"]`)
+					.should('have.data', 'consent')
+					.should('equal', true);
+			});
+		});
+
+		it(`should be able to refuse all but purpose 1`, () => {
+			cy.get('[data-cy=pm]').click();
+
+			cy.getIframeBody(iframePrivacyManager)
+				.find(`label[aria-label="Store and/or access information on a device"]`)
+				.find('span.on')
+				.click();
+
+			cy.getIframeBody(iframePrivacyManager).find(`div.stack-toggles`).click();
+
+			cy.getIframeBody(iframePrivacyManager)
+				.find(`button[aria-label="Save and close"]`)
+				.click();
+
+			// eslint-disable-next-line cypress/no-unnecessary-waiting
+			cy.wait(1000);
+
+			cy.get(`[data-purpose="1"]`)
+				.should('have.data', 'consent')
+				.should('equal', false);
+
+			[2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((purpose) => {
+				cy.get(`[data-purpose="${purpose}"]`)
+					.should('have.data', 'consent')
+					.should('equal', true);
+			});
 		});
 	});
-
-	it(`should be able to refuse all but purpose 1`, () => {
-		cy.get('[data-cy=pm]').click();
-
-		cy.getIframeBody(iframePrivacyManager)
-			.find(`label[aria-label="Store and/or access information on a device"]`)
-			.find('span.on')
-			.click();
-
-		cy.getIframeBody(iframePrivacyManager).find(`div.stack-toggles`).click();
-
-		cy.getIframeBody(iframePrivacyManager)
-			.find(`button[aria-label="Save and close"]`)
-			.click();
-
-		// eslint-disable-next-line cypress/no-unnecessary-waiting
-		cy.wait(1000);
-
-		cy.get(`[data-purpose="1"]`)
-			.should('have.data', 'consent')
-			.should('equal', false);
-
-		[2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((purpose) => {
-			cy.get(`[data-purpose="${purpose}"]`)
-				.should('have.data', 'consent')
-				.should('equal', true);
-		});
+});
+skipOn(Cypress.env('CI') !== 'true', () => {
+	describe('Skipped in CI', () => {
+		it('should skip TCFv2 in CI env, because of geolocation', () => true);
 	});
 });
