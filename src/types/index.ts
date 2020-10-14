c2d74eebea806c265440d1341ae8fb3758477b17
Max Duval <max@mxdvl.com>
2020-10-14 12:01:55 +0100

    Update README toc

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,8 @@
 import { CCPAConsentState, CustomVendorRejects } from './ccpa';
 import { TCFv2ConsentState } from './tcfv2';
 
+export type Framework = 'tcfv2' | 'ccpa' | 'aus';
+
 export interface ConsentState {
 	tcfv2?: TCFv2ConsentState;
 	ccpa?: CCPAConsentState;
@@ -20,8 +22,6 @@ export type WillShowPrivacyMessage = () => Promise<boolean>;
 
 export type Callback = (arg0: ConsentState) => void;
 export type CallbackQueueItem = { fn: Callback; lastState?: string };
-
-export type Framework = 'tcfv2' | 'ccpa' | 'aus';
 
 // https://documentation.sourcepoint.com/web-implementation/sourcepoint-gdpr-and-tcf-v2-support/__tcfapi-getcustomvendorconsents-api
 export interface VendorConsents {
