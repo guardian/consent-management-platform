5a05f237ddb12ffc7b0d4b99656f682858c82be3
Max Duval <max@mxdvl.com>
2020-10-21 15:46:53 +0100

    test: add tests for old interface

--- src/index.test.js
+++ src/index.test.js
@@ -158,3 +158,15 @@ describe('cmp.showPrivacyManager', () => {
 });
 
 it.todo('cmp.willShowPrivacyMessage');
+
+describe('Old API parameter `isInUsa`', () => {
+	it('Should handle `{ isInUsa: true }`', () => {
+		cmp.init({ isInUsa: true });
+		expect(CCPA.init).toHaveBeenCalledTimes(1);
+	});
+
+	it('Should handle `{ isInUsa: false }`', () => {
+		cmp.init({ isInUsa: false });
+		expect(TCFv2.init).toHaveBeenCalledTimes(1);
+	});
+});
