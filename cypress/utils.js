e744a9f2f8f217b332a348fc98ccf604f9db48bd
Alex Sanders <a@snd.rs>
2020-11-16 13:40:36 +0000

    fix: wait for sourcepoint library to load in aus (#354)
    * wait for sourcepoint library to load when
    
    * wait for uspapi to return to consider the library loaded
    
    * resolve loaded when sourcepoint consent fires
    
    * only strip console statements in prod
    
    * we now need to clear all cookies between tests
    
    * unify Cypress utils
    
    * clear cookies before visit
    
    Co-authored-by: Max Duval <max@mxdvl.com>

---
+++ cypress/utils.js
@@ -0,0 +1,9 @@
+export const loadPage = (url) => {
+	it(`should load the page: ${url}`, () => {
+		// undocumented fix for cookies from non-localhost domains
+		// https://github.com/cypress-io/cypress/issues/408#issuecomment-643281127
+		cy.clearCookies({ domain: null });
+
+		cy.visit(url);
+	});
+};
