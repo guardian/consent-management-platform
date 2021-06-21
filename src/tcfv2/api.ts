8f1f54664e62334e6b304230f686a5c842a19e32
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2021-06-21 11:05:29 +0100

    feat: Tcfapi callbacks (#464)
    * define tcfApiEventListener
    
    * tcfv2 uses tcfApiEventListener rather than onConsentReady
    
    * api function returns Promise<TCData>
    
    * invoke callbacks on useractioncomplete
    
    * remove onConsentReady assertion
    
    * new sp stub file
    
    * update comments
    
    * lint fix
    
    * rename tc-loaded -> got-consent
    
    * replace comment link
    
    * add comments to eventStatus switch

--- src/tcfv2/api.ts
+++ src/tcfv2/api.ts
@@ -1,3 +1,6 @@
+import { log } from '@guardian/libs';
+import { mark } from '../lib/mark';
+import { invokeCallbacks } from '../onConsentChange';
 import type { CustomVendorConsents } from '../types/tcfv2/CustomVendorConsents';
 import type { TCData } from '../types/tcfv2/TCData';
 
@@ -6,9 +9,9 @@ type Command =
 	| 'ping'
 	| 'addEventListener'
 	| 'removeEventListener'
-	| 'getCustomVendorConsents'; // sourecepoint addition https://documentation.sourcepoint.com/web-implementation/sourcepoint-gdpr-and-tcf-v2-support/__tcfapi-getcustomvendorconsents-api
+	| 'getCustomVendorConsents'; // Sourcepoint addition https://documentation.sourcepoint.com/web-implementation/sourcepoint-gdpr-and-tcf-v2-support/__tcfapi-getcustomvendorconsents-api
 
-const api = (command: Command) =>
+const api = (command: Command): Promise<TCData> =>
 	new Promise((resolve, reject) => {
 		if (window.__tcfapi) {
 			window.__tcfapi(command, 2, (result, success) =>
@@ -22,8 +25,36 @@ const api = (command: Command) =>
 		}
 	});
 
-export const getTCData = (): Promise<TCData> =>
-	api('getTCData') as Promise<TCData>;
+export const getTCData = (): Promise<TCData> => api('getTCData');
 
 export const getCustomVendorConsents = (): Promise<CustomVendorConsents> =>
-	api('getCustomVendorConsents') as Promise<CustomVendorConsents>;
+	api('getCustomVendorConsents') as unknown as Promise<CustomVendorConsents>;
+
+export const tcfApiEventListener = (): void => {
+	// https://documentation.sourcepoint.com/api/gdpr-tcf-v2-api/the-__tcfapi-gettcdata-api-overview/using-__tcfapi-addeventlistener-and-removeeventlistener-commands
+	if (window.__tcfapi) {
+		window.__tcfapi('addEventListener', 2, (result, success) => {
+			const { eventStatus } = result;
+			log('cmp', 'Tcf api event:', eventStatus);
+
+			if (!success) {
+				log('cmp', 'Tcf api addEventListener failed:');
+			}
+
+			switch (eventStatus) {
+				case 'tcloaded':
+					// This is the event status when a TC String is available to any calling scripts on the page.
+					mark('cmp-tcfv2-got-consent');
+					invokeCallbacks();
+					break;
+				case 'useractioncomplete':
+					// This is the event status whenever a user has confirmed or re-confirmed their choices.
+					mark('cmp-tcfv2-user-action-complete');
+					invokeCallbacks();
+					break;
+			}
+		});
+	} else {
+		console.warn('No __tcfapi found on window');
+	}
+};
