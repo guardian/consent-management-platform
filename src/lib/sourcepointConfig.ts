452e6db2d7b27cc5083187ee99f413142411dfdd
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2021-09-06 15:35:01 +0100

    update privacy manager IDs

--- src/lib/sourcepointConfig.ts
+++ src/lib/sourcepointConfig.ts
@@ -1,9 +1,9 @@
 import { isGuardianDomain } from './domain';
 
 export const ACCOUNT_ID = 1257;
-export const PRIVACY_MANAGER_CCPA = 533907;
+export const PRIVACY_MANAGER_CCPA = 540252;
 export const PRIVACY_MANAGER_TCFV2 = 106842;
-export const PRIVACY_MANAGER_AUSTRALIA = 1; // TODO create privacy manager and replace ID
+export const PRIVACY_MANAGER_AUSTRALIA = 540341;
 
 export const ENDPOINT = isGuardianDomain()
 	? 'https://sourcepoint.theguardian.com'
