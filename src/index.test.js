e7c40b9e046b4ccad774c6994890ff8913dc6177
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2021-10-21 11:48:00 +0100

    Remove isInUsa unit tests?

--- src/index.test.js
+++ src/index.test.js
@@ -188,21 +188,3 @@ describe('cmp.showPrivacyManager', () => {
 		);
 	});
 });
-
-describe('Old API parameter `isInUsa`', () => {
-	it('Should handle `{ isInUsa: true }`', () => {
-		cmp.init({ isInUsa: true });
-		expect(CMP.init).toHaveBeenCalledTimes(1);
-	});
-
-	it('Should handle `{ isInUsa: false }`', () => {
-		cmp.init({ isInUsa: false });
-		expect(CMP.init).toHaveBeenCalledTimes(1);
-	});
-
-	it('Should throw an error if neither is passed', () => {
-		expect(() => {
-			cmp.init({});
-		}).toThrow('required');
-	});
-});
