9392e9240b302bfef38169a7b14b83579ea6b503
Max Duval <max.duval@theguardian.com>
2021-12-06 12:30:24 +0000

    fix: Wait for user action to trigger callbacks in TCFv2 (#535)
    * wait for user action to trigger callbacks in TCFv2
    
    * add test around `cmpuishown`

--- src/onConsentChange.ts
+++ src/onConsentChange.ts
@@ -7,7 +7,17 @@ import type { Callback, CallbackQueueItem, ConsentState } from './types';
 // callbacks cache
 const callBackQueue: CallbackQueueItem[] = [];
 
+/**
+ * In TCFv2, check whether the event status anything but `cmpuishown`, i.e.:
+ * - `useractioncomplete`
+ * - `tcloaded`
+ */
+const awaitingUserInteractionInTCFv2 = (state: ConsentState): boolean =>
+	state.tcfv2?.eventStatus === 'cmpuishown';
+
 const invokeCallback = (callback: CallbackQueueItem, state: ConsentState) => {
+	if (awaitingUserInteractionInTCFv2(state)) return;
+
 	const stateString = JSON.stringify(state);
 
 	// only invoke callback if the consent state has changed
@@ -34,6 +44,8 @@ const getConsentState: () => Promise<ConsentState> = async () => {
 export const invokeCallbacks = (): void => {
 	if (callBackQueue.length === 0) return;
 	void getConsentState().then((state) => {
+		if (awaitingUserInteractionInTCFv2(state)) return;
+
 		callBackQueue.forEach((callback) => invokeCallback(callback, state));
 	});
 };
@@ -44,7 +56,7 @@ export const onConsentChange: (fn: Callback) => void = (callBack) => {
 	callBackQueue.push(newCallback);
 
 	// if consentState is already available, invoke callback immediately
-	getConsentState()
+	void getConsentState()
 		.then((consentState) => {
 			invokeCallback(newCallback, consentState);
 		})
