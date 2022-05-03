c5ca60aafec6874705a81ed1396e459d550866b7
Pascal Honoré <pascal@alseyn.net>
2022-05-03 15:50:59 +0100

    pascal: experiment: trying to establish what the problem is in AU (2)

--- cdk/lib/monitoring.ts
+++ cdk/lib/monitoring.ts
@@ -4,7 +4,7 @@ import { Duration } from '@aws-cdk/core';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
-import { GuSnsTopic } from '@guardian/cdk/lib/constructs/sns';
+// import { GuSnsTopic } from '@guardian/cdk/lib/constructs/sns';
 
 export class Monitoring extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
@@ -16,18 +16,14 @@ export class Monitoring extends GuStack {
 
 		const lambdaBaseName = 'cmp-monitoring';
 
-		if (region === 'ap-southeast-2') {
-			new GuSnsTopic(this, 'SnsTopic');
-		} else {
-			new GuLambdaFunction(this, lambdaBaseName, {
-				app: `${lambdaBaseName}-lambda-${region}`,
-				functionName: `${lambdaBaseName}-${stage}`,
-				fileName: `${lambdaBaseName}-lambda-${region}.zip`,
-				handler: 'index.handler',
-				runtime: Runtime.NODEJS_14_X,
-				timeout: Duration.seconds(300),
-				memorySize: 1024,
-			});
-		}
+		new GuLambdaFunction(this, lambdaBaseName, {
+			app: `${lambdaBaseName}-lambda-${region}`,
+			functionName: `${lambdaBaseName}-${stage}`,
+			fileName: `${lambdaBaseName}-lambda-${region}.zip`,
+			handler: 'index.handler',
+			runtime: Runtime.NODEJS_14_X,
+			timeout: Duration.seconds(300),
+			memorySize: 1024,
+		});
 	}
 }
