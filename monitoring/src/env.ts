5a818ef3614096ef723aed8e7cf23053efb98010
Pascal Honoré <pascal@alseyn.net>
2022-05-16 15:13:07 +0100

    Rename configRegion to configJurisdiction to clarify the difference between AWS regions and legal jurisdictions

--- monitoring/src/env.ts
+++ monitoring/src/env.ts
@@ -1,3 +1,3 @@
 export const debugMode: boolean = process.env['DEBUG_MODE'] == 'true';
 export const configStage: string = process.env['CMP_ENV'] ?? 'prod';
-export const configRegion: string = process.env['CMP_REGION'] ?? 'tcfv2';
+export const configJurisdiction: string = process.env['CMP_REGION'] ?? 'tcfv2';
