dd2bc1e6e4e143fb0dc4398cfd75ea2bf62dde6e
Pascal Honoré <pascal@alseyn.net>
2022-05-16 15:30:33 +0100

    Update environment variables names

--- monitoring/src/env.ts
+++ monitoring/src/env.ts
@@ -1,3 +1,4 @@
 export const debugMode: boolean = process.env['DEBUG_MODE'] == 'true';
-export const configStage: string = process.env['CMP_ENV'] ?? 'prod';
-export const configJurisdiction: string = process.env['CMP_REGION'] ?? 'tcfv2';
+export const configStage: string = process.env['STAGE'] ?? 'prod';
+export const configJurisdiction: string =
+	process.env['CMP_JURISDICTION'] ?? 'tcfv2';
