78a64722b5936f17b099d4f2708dd0e472a1ca38
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2022-06-22 13:38:12 +0100

    Create ConfigManager.ts

---
+++ monitoring/src/utils/ConfigManager.ts
@@ -0,0 +1,112 @@
+import type { Config } from '../types';
+import { mainCheck as mainCheckAus } from '../check-page/aus';
+import { mainCheck as mainCheckCCPA } from '../check-page/ccpa';
+import { mainCheck as mainCheckTcfV2 } from '../check-page/tcfv2';
+import { E_Jurisdiction, E_Stage } from '../enums';
+
+
+
+class ConfigManager {
+
+	static getEnvConfig = (stage: E_Stage, jurisdiction: E_Jurisdiction) : Config => {
+		switch(jurisdiction){
+			case E_Jurisdiction.AUS:
+				return ConfigManager.getAus(stage);
+			case E_Jurisdiction.TCFV2:
+				return ConfigManager.getTcfv(stage);
+			case E_Jurisdiction.CCPA:
+				return ConfigManager.getCCPA(stage);
+		}
+	}
+
+
+	private static getAus = (stage: E_Stage) : Config => {
+		switch(stage){
+			case E_Stage.CODE:
+				return ConfigAusCode;
+			case E_Stage.PROD:
+				return ConfigAusProd;
+		}
+	}
+
+	private static getTcfv = (stage: E_Stage) : Config => {
+		switch(stage){
+			case E_Stage.CODE:
+				return ConfigTcfv2Code;
+			case E_Stage.PROD:
+				return ConfigTcfv2Prod;
+		}
+	}
+
+	static getCCPA = (stage: E_Stage) : Config => {
+		switch(stage){
+			case E_Stage.CODE:
+				return ConfigCCPACode;
+			case E_Stage.PROD:
+				return ConfigCCPAProd;
+		}
+	}
+}
+
+
+
+const ConfigTcfv2Prod: Config = {
+	stage: 'prod',
+	jurisdiction: 'tcfv2',
+	frontUrl: 'https://www.theguardian.com',
+	articleUrl:
+		'https://www.theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake',
+	iframeDomain: 'https://sourcepoint.theguardian.com',
+	checkFunction: mainCheckTcfV2,
+};
+
+const ConfigTcfv2Code: Config = {
+	stage: 'code',
+	jurisdiction: 'tcfv2',
+	frontUrl: 'https://m.code.dev-theguardian.com',
+	articleUrl:
+		'https://m.code.dev-theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake',
+	iframeDomain: 'https://cdn.privacy-mgmt.com',
+	checkFunction: mainCheckTcfV2,
+};
+
+const ConfigCCPAProd: Config = {
+	stage: 'prod',
+	jurisdiction: 'ccpa',
+	frontUrl: 'https://www.theguardian.com/us',
+	articleUrl:
+		'https://www.theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake',
+	iframeDomain: 'https://ccpa-notice.sp-prod.net',
+	checkFunction: mainCheckCCPA,
+};
+
+const ConfigCCPACode: Config = {
+	stage: 'code',
+	jurisdiction: 'ccpa',
+	frontUrl: 'https://m.code.dev-theguardian.com/us',
+	articleUrl:
+		'https://m.code.dev-theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake',
+	iframeDomain: 'https://ccpa-notice.sp-prod.net',
+	checkFunction: mainCheckCCPA,
+};
+
+const ConfigAusProd: Config = {
+	stage: 'prod',
+	jurisdiction: 'aus',
+	frontUrl: 'https://www.theguardian.com/au',
+	articleUrl:
+		'https://www.theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake',
+	iframeDomain: 'https://ccpa-notice.sp-prod.net',
+	checkFunction: mainCheckAus,
+};
+
+const ConfigAusCode: Config = {
+	stage: 'code',
+	jurisdiction: 'aus',
+	frontUrl: 'https://m.code.dev-theguardian.com/au',
+	articleUrl:
+		'https://m.code.dev-theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake',
+	iframeDomain: 'https://ccpa-notice.sp-prod.net',
+	checkFunction: mainCheckAus,
+};
+
