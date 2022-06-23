e6b4b7560eadd22557556fb998ab8cb180071d62
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2022-06-23 09:49:30 +0100

    Update monitoring.ts

--- cdk/lib/monitoring.ts
+++ cdk/lib/monitoring.ts
@@ -1,3 +1,5 @@
+import { Rule, Schedule } from '@aws-cdk/aws-events';
+import { LambdaFunction } from '@aws-cdk/aws-events-targets';
 import { Runtime } from '@aws-cdk/aws-lambda';
 import type { App } from '@aws-cdk/core';
 import { Duration } from '@aws-cdk/core';
@@ -15,14 +17,27 @@ export class Monitoring extends GuStack {
 
 		const lambdaBaseName = 'cmp-monitoring';
 
-		new GuLambdaFunction(this, lambdaBaseName, {
-			app: `${lambdaBaseName}-lambda-${region}`,
-			functionName: `${lambdaBaseName}-${stage}`,
-			fileName: `${lambdaBaseName}-lambda-${region}.zip`,
-			handler: 'index.handler',
-			runtime: Runtime.NODEJS_14_X,
-			timeout: Duration.seconds(300),
-			memorySize: 2048,
+		const monitoringLambdaFunction = new GuLambdaFunction(
+			this,
+			lambdaBaseName,
+			{
+				app: `${lambdaBaseName}-lambda-${region}`,
+				functionName: `${lambdaBaseName}-${stage}`,
+				fileName: `${lambdaBaseName}-lambda-${region}.zip`,
+				handler: 'index.handler',
+				runtime: Runtime.NODEJS_14_X,
+				timeout: Duration.seconds(300),
+				memorySize: 2048,
+			},
+		);
+
+		// monitoringLambdaFunction.addEventSource()
+
+		// Add event source
+		const lambdaEventTarget = new LambdaFunction(monitoringLambdaFunction);
+		new Rule(this, 'cmp monitoring schedule', {
+			schedule: Schedule.cron({ hour: '23', minute: '4' }),
+			targets: [lambdaEventTarget],
 		});
 	}
 }
