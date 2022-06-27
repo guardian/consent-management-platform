3d1f727ca63810c6732b800aa9e09744e0bd8f69
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2022-06-27 16:49:35 +0100

    Update localRun.ts

--- monitoring/localRun.ts
+++ monitoring/localRun.ts
@@ -1,6 +1,36 @@
+import { prompt } from 'inquirer';
+import type { CustomScheduleEventContent } from './src';
+
+type UserInput = {
+	stage: string;
+	jurisdiction: string[];
+};
 async function main() {
 	const { handler } = await import('./src');
-	handler().catch(console.error);
+
+	await prompt([
+		{
+			type: 'list',
+			name: 'stage',
+			message: 'Which environment would you like to test?',
+			choices: ['code', 'prod'],
+		},
+		{
+			type: 'checkbox',
+			name: 'jurisdiction',
+			message:
+				'Which jurisdiction would you like to test? (Use the up and down keys to move ,space bar to select the jurisdiction)',
+			choices: ['aus', 'ccpa', 'tcfv2'],
+		},
+	]).then((userInput: UserInput) => {
+		userInput.jurisdiction.forEach((jurisdiction) => {
+			const event: CustomScheduleEventContent = {
+				stage: userInput.stage,
+				jurisdiction: jurisdiction,
+			};
+			handler(event).catch(console.error);
+		});
+	});
 }
 
 void main();
