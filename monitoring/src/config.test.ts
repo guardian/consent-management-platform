4e826c19a28e639b876a09fb29e0388994429249
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2022-07-04 10:21:06 +0100

    Update config.test.ts
    Added new tests

--- monitoring/src/config.test.ts
+++ monitoring/src/config.test.ts
@@ -1,10 +1,15 @@
 import { ConfigWrapper } from './config';
+import {
+	AWS_REGIONS,
+	ConfigHelper,
+	JURISDICTIONS,
+} from './utils/config-helper';
 
 describe('ConfigWrapper', () => {
 	describe('Constructor', () => {
-		const awsRegion = 'us-west-1';
-		const stage = 'us-west-1';
-		const jurisdiction = 'tcfv2';
+		const awsRegion = AWS_REGIONS.US_WEST_1;
+		const stage = 'code';
+		const jurisdiction = JURISDICTIONS.AUS;
 		it('should assign jurisdiction, awsRegion or stage passed', () => {
 			const configWrapper = new ConfigWrapper(
 				awsRegion,
@@ -15,6 +20,51 @@ describe('ConfigWrapper', () => {
 			expect(configWrapper.jurisdiction).toBe(jurisdiction);
 			expect(configWrapper.stage).toBe(stage);
 			expect(configWrapper.awsRegion).toBe(awsRegion);
+		});
+	});
+
+	describe('generateConfig', () => {
+		const awsRegion = AWS_REGIONS.US_WEST_1;
+		const stage = 'code';
+		const jurisdiction = JURISDICTIONS.CCPA;
+		it('should assign jurisdiction if there is an awsRegion and no jurisdiction ', () => {
+			const configWrapper = new ConfigWrapper(
+				awsRegion,
+				stage,
+				undefined,
+			);
+
+			expect(configWrapper.jurisdiction).toBeUndefined();
+			expect(configWrapper.config).toBeUndefined();
+			expect(configWrapper.stage).toBe(stage);
+			expect(configWrapper.awsRegion).toBe(awsRegion);
+
+			configWrapper.generateConfig();
+
+			expect(configWrapper.jurisdiction).toBe(
+				ConfigHelper.getJurisdiction(awsRegion),
+			);
+			expect(configWrapper.config).not.toBeUndefined();
+		});
+
+		it('should assign awsRegion if there is an jurisdiction and no awsRegion ', () => {
+			const configWrapper = new ConfigWrapper(
+				undefined,
+				stage,
+				jurisdiction,
+			);
+
+			expect(configWrapper.awsRegion).toBeUndefined();
+			expect(configWrapper.config).toBeUndefined();
+			expect(configWrapper.stage).toBe(stage);
+			expect(configWrapper.jurisdiction).toBe(jurisdiction);
+
+			configWrapper.generateConfig();
+
+			expect(configWrapper.awsRegion).toBe(
+				ConfigHelper.getRegion(jurisdiction),
+			);
+			expect(configWrapper.config).not.toBeUndefined();
 		});
 	});
 });
