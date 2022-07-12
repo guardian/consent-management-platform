86088a82f6ef505d4e9308f3972bd444d0f32e1d
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2022-07-12 09:28:02 +0100

    Creating ConfigManager that can be extended
    Creating

--- monitoring/src/utils/jurisdiction-config-manager/jurisdiction-config-manager.ts
+++ monitoring/src/utils/jurisdiction-config-manager/jurisdiction-config-manager.ts
@@ -1,31 +1,93 @@
 import { mainCheck as mainCheckAus } from '../../check-page/aus';
 import { mainCheck as mainCheckCCPA } from '../../check-page/ccpa';
 import { mainCheck as mainCheckTcfV2 } from '../../check-page/tcfv2';
+import { debugMode } from '../../env';
 import type { Config, Jurisdiction, Stage } from '../../types';
-import { STAGES } from '../config-helper/config-helper';
+import { JURISDICTIONS, STAGES } from '../config-helper/config-helper';
 
 export class JurisdictionConfigManager {
 	static constructConfig(stage: Stage, jurisdiction: Jurisdiction): Config {
 		const config: Config = {
 			stage: stage,
 			jurisdiction: jurisdiction,
-			frontUrl: this.getFrontUrl(stage, jurisdiction),
+			frontUrl: this.getFrontUrl(stage),
+			articleUrl: this.getArticleUrl(stage),
+			iframeDomain: this.getIframeDomain(stage),
+			checkFunction: this.getCheckFunction(jurisdiction),
+			debugMode: debugMode,
 		};
 
 		return config;
 	}
 
-	static getFrontUrl(stage: Stage, jurisdiction: Jurisdiction): string {
-		const baseUrl: string = '';
+	static getFrontUrl(stage: Stage): string {
 		switch (stage) {
 			case STAGES.PROD:
-				break;
+				return ConfigFrontUrl.PROD;
 			case STAGES.CODE:
-				break;
+				return ConfigFrontUrl.CODE;
 			case STAGES.LOCAL:
-				break;
+				return ConfigFrontUrl.LOCAL;
+			default:
+				return ConfigFrontUrl.CODE;
 		}
+	}
 
-		return baseUrl;
+	static getArticleUrl(stage: Stage): string {
+		switch (stage) {
+			case STAGES.PROD:
+				return ConfigArticleUrl.PROD;
+			case STAGES.CODE:
+				return ConfigArticleUrl.CODE;
+			case STAGES.LOCAL:
+				return ConfigArticleUrl.LOCAL;
+			default:
+				return ConfigArticleUrl.CODE;
+		}
+	}
+
+	static getIframeDomain(stage: Stage): string {
+		switch (stage) {
+			case STAGES.PROD:
+				return IframeDomainUrl.PROD;
+			case STAGES.CODE:
+				return IframeDomainUrl.CODE;
+			case STAGES.LOCAL:
+				return IframeDomainUrl.LOCAL;
+			default:
+				return IframeDomainUrl.CODE;
+		}
+	}
+	static getCheckFunction(
+		jurisdiction: Jurisdiction,
+	): (config: Config) => Promise<void> {
+		switch (jurisdiction) {
+			case JURISDICTIONS.AUS:
+				return mainCheckAus;
+			case JURISDICTIONS.CCPA:
+				return mainCheckCCPA;
+			case JURISDICTIONS.TCFV2:
+				return mainCheckTcfV2;
+			default:
+				return mainCheckTcfV2;
+		}
 	}
 }
+
+export const ConfigArticleUrl = {
+	PROD: 'https://www.theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake',
+	CODE: 'https://m.code.dev-theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake',
+	LOCAL: 'https://m.code.dev-theguardian.com/food/2020/dec/16/how-to-make-the-perfect-vegetarian-sausage-rolls-recipe-felicity-cloake',
+};
+
+export const ConfigFrontUrl = {
+	PROD: 'https://www.theguardian.com',
+	CODE: 'https://m.code.dev-theguardian.com',
+	LOCAL: 'https://m.code.dev-theguardian.com',
+};
+
+export const IframeDomainUrl = {
+	PROD: 'https://sourcepoint.theguardian.com',
+	CODE: 'https://sourcepoint.theguardian.com',
+	LOCAL: 'https://sourcepoint.theguardian.com',
+};
