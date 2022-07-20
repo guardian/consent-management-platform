f063e491e089ec7c9edce2a32592a88c035b4eff
Georges Lebreton <georges.lebreton@guardian.co.uk>
2022-07-20 12:41:08 +0100

    updated test

--- cdk/lib/monitoring.test.ts
+++ cdk/lib/monitoring.test.ts
@@ -1,6 +1,6 @@
 import '@aws-cdk/assert/jest';
 import { SynthUtils } from '@aws-cdk/assert';
-import { App } from '@aws-cdk/core';
+import { App, Stage } from '@aws-cdk/core';
 import { Monitoring } from './monitoring';
 
 describe('The Monitoring stack', () => {
@@ -8,6 +8,7 @@ describe('The Monitoring stack', () => {
 		const app = new App();
 		const stack = new Monitoring(app, 'CmpMonitoringStack', {
 			stack: 'cmp-monitoring',
+			stage: 'PROD',
 		});
 		expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 	});
