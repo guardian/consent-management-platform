588fb43bbe0f9154eb9f64ce8484b0b7143344c8
Robert Kenny <robert.kenny@guardian.co.uk>
2022-07-22 12:45:42 +0100

    fix deps

--- cdk/lib/monitoring.test.ts
+++ cdk/lib/monitoring.test.ts
@@ -1,6 +1,4 @@
-import '@aws-cdk/assert/jest';
-import { SynthUtils } from '@aws-cdk/assert';
-import { App } from '@aws-cdk/core';
+import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { Monitoring } from './monitoring';
 
