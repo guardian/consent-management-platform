694d67e2bcc2a289b66317c727f3682c0ce05ef7
Georges Lebreton <102960844+Georges-GNM@users.noreply.github.com>
2022-08-12 14:16:00 +0100

    chore: Flaky cypress test improvement (#656)
    * bumped cypress to latest version, tweaked rtries and timeout values
    
    * removed local shell scripts
    
    * tried pointing eslint to cypress config file
    
    * trying to resolve lint issue
    
    * fix linting issue
    
    * added caret to cypress version
    
    * tried to resolve yarn lock issue
    
    * Tweaked retry and timeout options

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,16 @@ module.exports = {
 	},
 	extends: ['@guardian/eslint-config-typescript'],
 	globals: { __PACKAGE_VERSION__: 'readonly' },
-	ignorePatterns: ['dist', 'coverage', 'cdk', 'monitoring'],
+	ignorePatterns: ['dist', 'coverage', 'cdk', 'monitoring', '*.ts', '*.*.ts'],
 	rules: {
 		// a lot of the 3rd party code uses these
 		'no-underscore-dangle': 0,
 	},
+
 	overrides: [
 		{
 			// tests and config files sometimes do unorthodox things, and that's ok
-			files: ['**/*.test.*', '*.config.js'],
+			files: ['**/*.test.*', '*.config.js', '*.ts'],
 			rules: {
 				'@typescript-eslint/no-unsafe-return': 0,
 				'@typescript-eslint/no-unsafe-call': 0,
