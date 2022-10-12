84a9bd3deb3834758dbf4ea7979654c6900087b6
Sue <sookburt@gmail.com>
2022-10-12 15:55:15 +0100

    using static file to test for existance of frames

--- monitoring/src/check-page/common-functions.test.ts
+++ monitoring/src/check-page/common-functions.test.ts
@@ -3,24 +3,26 @@
  */
 
 import 'expect-puppeteer';
+import path from 'path';
 import type { Browser, Frame, Page } from 'puppeteer-core';
-import {
-	ConfigFrontUrl,
-	IframeDomainUrl,
-} from '../utils/config-builder/config-builder';
+import { IframeDomainUrl } from '../utils/config-builder/config-builder';
 import { getFrame, makeNewBrowser } from './common-functions';
 
 describe('common-functions.ts', () => {
 	jest.setTimeout(20000);
-	//const frontUrl = ConfigFrontUrl.CODE;
-	const iframeDomainUrl = IframeDomainUrl.CODE;
-	let page: Page;
+	const firstLayerIframeDomain = IframeDomainUrl.CODE;
+	const secondLayerIframeDomain = IframeDomainUrl.CODE + '/privacy-manager';
 	let browser: Browser;
+	let page: Page;
+	const staticFile = `file:${path.join(
+		__dirname,
+		'../../static-test/frame-test.html',
+	)}`;
 	describe('getFrame', () => {
 		beforeAll(async () => {
 			browser = await makeNewBrowser(false);
 			page = await browser.newPage();
-			await page.goto(iframeDomainUrl);
+			await page.goto(staticFile);
 		});
 
 		afterAll(async () => {
@@ -28,8 +30,13 @@ describe('common-functions.ts', () => {
 			await browser.close();
 		});
 
-		it('should return a frame', () => {
-			const frame: Frame = getFrame(page, iframeDomainUrl);
+		it('should return a 1st layer frame in code', () => {
+			const frame: Frame = getFrame(page, firstLayerIframeDomain);
+			expect(frame).toBeDefined();
+		});
+
+		it('should return a 2nd layer frame in code', () => {
+			const frame: Frame = getFrame(page, secondLayerIframeDomain);
 			expect(frame).toBeDefined();
 		});
 	});
