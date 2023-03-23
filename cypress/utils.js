098c971b5515a3892c7c096f2af2a2534a784bc6
Ravi <7014230+arelra@users.noreply.github.com>
2023-03-23 12:52:50 +0000

    Upgrade cypress@^12.8.1

--- cypress/utils.js
+++ cypress/utils.js
@@ -2,9 +2,7 @@ export const loadPage = (url) => {
 	it(`should load the page: ${url}`, () => {
 		cy.clearLocalStorage();
 
-		// undocumented fix for cookies from non-localhost domains
-		// https://github.com/cypress-io/cypress/issues/408#issuecomment-643281127
-		cy.clearCookies({ domain: null });
+		cy.clearAllCookies();
 
 		cy.visit(url);
 	});
