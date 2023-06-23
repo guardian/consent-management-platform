613344bc0d7f1870cb6b10b15cd925044b98d9ed
Anna Voelker <anna.voelker@guardian.co.uk>
2023-06-23 10:31:59 +0100

    test: remove promise.all

--- monitoring/src/check-page/aus.ts
+++ monitoring/src/check-page/aus.ts
@@ -34,10 +34,10 @@ const checkSubsequentPage = async (browser: Browser, url: string) => {
 	log_info(`Start checking subsequent Page URL: ${url}`);
 	const page: Page = await browser.newPage();
 	await loadPage(page, url);
-	await Promise.all([
-		checkCMPIsNotVisible(page),
-		checkTopAdHasLoaded(page),
-	]);
+	//await Promise.all([
+		await checkCMPIsNotVisible(page);//,
+		await checkTopAdHasLoaded(page);//,
+	//]);
 	await page.close();
 	log_info(`Checking subsequent Page URL: ${url} Complete`);
 };
