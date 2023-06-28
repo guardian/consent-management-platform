909298eb3b43bde5a415017f7a3bbd1635bc8df3
Anna Voelker <anna.voelker@guardian.co.uk>
2023-06-28 22:47:38 +0100

    test: domcontent

--- monitoring/src/check-page/common-functions.ts
+++ monitoring/src/check-page/common-functions.ts
@@ -276,7 +276,7 @@ export const loadPage = async (page: Page, url: string): Promise<void> => {
 	await page.setCacheEnabled(false);
 
 	const response = await page.goto(url, {
-		waitUntil: 'networkidle2',
+		waitUntil: 'domcontentloaded',
 		timeout: 30000,
 	});
 
@@ -301,7 +301,7 @@ export const loadPage = async (page: Page, url: string): Promise<void> => {
 export const reloadPage = async (page: Page) => {
 	log_info(`Reloading page: Start`);
 	const reloadResponse = await page.reload({
-		waitUntil: ['networkidle2'],
+		waitUntil: ['domcontentloaded'],
 		timeout: 30000,
 	});
 	if (!reloadResponse) {
