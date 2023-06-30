e7c7e89dfdc01142d4226ac853f5d0624692561b
Anna Voelker <anna.voelker@guardian.co.uk>
2023-06-30 14:31:29 +0100

    test: slowmo 500

--- monitoring/src/check-page/common-functions.ts
+++ monitoring/src/check-page/common-functions.ts
@@ -96,7 +96,7 @@ const launchBrowser = async (ops: CustomPuppeteerOptions): Promise<Browser> => {
  * @param {boolean} debugMode
  * @return {*}  {Promise<Browser>}
  */
-export const makeNewBrowser = async (debugMode: boolean, slowMo: number = 200): Promise<Browser> => {
+export const makeNewBrowser = async (debugMode: boolean, slowMo: number = 500): Promise<Browser> => {
 	chromium.setGraphicsMode = false; //required for browser.close() not to hang
 	const ops = await initialiseOptions(debugMode, slowMo);
 	const browser = await launchBrowser(ops);
