7eed5ac0ad533f66d2a89b39c036e22f2c3e810d
Anna Voelker <anna.voelker@guardian.co.uk>
2023-07-03 17:33:45 +0100

    test: both back

--- src/vendors.ts
+++ src/vendors.ts
@@ -25,7 +25,7 @@ export const VendorIDs = {
 	ias: ['5e7ced57b8e05c485246ccf3'],
 	inizio: ['5e37fc3e56a5e6615502f9c9'],
 	ipsos: ['5fa51b29a228638b4a1980e4'],
-	//lotame: ['5ed6aeb1b8e05c241a63c71f'],
+	lotame: ['5ed6aeb1b8e05c241a63c71f'],
 	nielsen: ['5ef5c3a5b8e05c69980eaa5b'],
 	ophan: ['5f203dbeeaaaa8768fd3226a'],
 	permutive: ['5eff0d77969bfa03746427eb'],
@@ -34,7 +34,7 @@ export const VendorIDs = {
 	redplanet: ['5f199c302425a33f3f090f51'],
 	remarketing: ['5ed0eb688a76503f1016578f'],
 	sentry: ['5f0f39014effda6e8bbd2006'],
-	//teads: ['5eab3d5ab8e05c2bbe33f399'],
+	teads: ['5eab3d5ab8e05c2bbe33f399'],
 	twitter: ['5e71760b69966540e4554f01'],
 	'youtube-player': ['5e7ac3fae30e7d1bc1ebf5e8'],
 };
