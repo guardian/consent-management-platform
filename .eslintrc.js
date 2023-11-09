4e47809eb95bd981e9c7353262cf64e213a79397
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-11-09 16:11:47 +0000

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,29 @@ module.exports = {
 		jest: true,
 		browser: true,
 	},
-	extends: ['@guardian/eslint-config-typescript'],
 	globals: { __PACKAGE_VERSION__: 'readonly' },
 	ignorePatterns: ['dist', 'coverage', 'cdk', 'monitoring', '*.ts', '*.*.ts'],
 	rules: {
 		// a lot of the 3rd party code uses these
 		'no-underscore-dangle': 0,
 	},
-
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 6,
+		project: 'tsconfig.json',
+		sourceType: 'module',
+	},
 	overrides: [
 		{
-			// tests and config files sometimes do unorthodox things, and that's ok
-			files: ['**/*.test.*', '*.config.js'],
+			files: ['*.js', '*.jsx'],
+			extends: ['@guardian/eslint-config'],
+		},
+		{
+			files: ['*.ts', '*.tsx'],
+			extends: ['@guardian/eslint-config-typescript'],
+		},
+		{
+			files: ['**/*.test.*'],
 			rules: {
 				'@typescript-eslint/no-unsafe-return': 0,
 				'@typescript-eslint/no-unsafe-call': 0,
