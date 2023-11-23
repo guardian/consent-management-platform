86fcdefe7bed93b66433a3c28af8755a2f946f79
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-11-23 17:20:21 +0000

    Update monitoring.ts

--- cdk/lib/monitoring.ts
+++ cdk/lib/monitoring.ts
@@ -88,7 +88,9 @@ export class Monitoring extends GuStack {
 			comparisonOperator:
 				ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
 			threshold: 4,
-			evaluationPeriods: 5,
+			evaluationPeriods: 10,
+			actionsEnabled: true,
+			datapointsToAlarm: 4,
 			metric: errorMetric,
 			alarmDescription:
 				'Alarm if the SUM of Errors is greater than or equal to the threshold (4) for 5 evaluation period',
