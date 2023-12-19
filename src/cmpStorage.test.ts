81a9853c3d58c60c8437043903a4aa5ed1ad16f8
Anna Voelker <anna.voelker@guardian.co.uk>
2023-12-19 13:00:38 +0000

    add cookie tests

--- src/cmpStorage.test.ts
+++ src/cmpStorage.test.ts
@@ -1,9 +1,8 @@
 import { onConsentChange } from './onConsentChange';
 import type { Callback, ConsentState } from './types';
 import type { TCFv2ConsentState } from './types/tcfv2';
-import {cmpGetLocalStorageItem, cmpSetLocalStorageItem, cmpGetSessionStorageItem, cmpSetSessionStorageItem, _private} from './cmpStorage';
-//import { getCookie as getCookie_ } from 'lib/cookies';
-import { storage as storageStub } from '@guardian/libs';
+import {cmpGetLocalStorageItem, cmpSetLocalStorageItem, cmpGetSessionStorageItem, cmpSetSessionStorageItem, cmpGetCookie, cmpSetCookie, _private } from './cmpStorage';
+import { getCookie as getCookie_, setCookie as setCookie_, storage as storageStub} from '@guardian/libs';
 
 jest.mock('./onConsentChange');
 jest.mock('@guardian/libs', () => ({
@@ -209,4 +208,70 @@ describe('session storage returns the expected consent', () => {
 
 
 describe('cookies return the expected consent', () => {
+	let mockContains:any;
+
+	beforeEach(() => {
+        mockContains = 'someTestData';
+
+        (getCookie_ as jest.Mock).mockImplementation(({name }: {
+			name: string;
+		}) => {
+			if (name === 'gu.mock') {return mockContains}
+			else {return(null)}
+        });
+
+        (setCookie_ as jest.Mock).mockImplementation(({ name, value }: {
+			name: string;
+			value: string;
+		}) => {
+            if (name === 'gu.mock') {mockContains = value;}
+        });
+    });
+
+    test('Targeted advertising get cookie returns null when canTarget is false', async () => {
+        const consentState: ConsentState = {
+            tcfv2: tcfv2ConsentState,
+            canTarget: false,
+            framework: 'tcfv2',
+        };
+        mockOnConsentChange(consentState);
+        const cookieValue = await cmpGetCookie({useCase: 'Targeted advertising', name: 'gu.mock'});
+        expect(cookieValue).toEqual(null);
+    });
+	test('Targeted advertising can set and get cookies when canTarget is true', async () => {
+        const consentState: ConsentState = {
+            tcfv2: tcfv2ConsentState,
+            canTarget: true,
+            framework: 'tcfv2',
+        };
+        mockOnConsentChange(consentState);
+		const cookieValueDefault = await cmpGetCookie({useCase: 'Targeted advertising', name: 'gu.mock'});
+        expect(cookieValueDefault).toEqual('someTestData');
+		await cmpSetCookie({useCase: 'Essential', name: 'gu.mock', value: 'testdataAd'});
+        const cookieValue = await cmpGetCookie({useCase: 'Targeted advertising', name: 'gu.mock'});
+        expect(cookieValue).toEqual('testdataAd');
+    });
+    test('Essential can set and get cookies when no consents', async () => {
+        const consentState: ConsentState = {
+            tcfv2: tcfv2ConsentStateNoConsent,
+            canTarget: false,
+            framework: 'tcfv2',
+        };
+        mockOnConsentChange(consentState);
+		const cookieValueDefault = await cmpGetCookie({useCase: 'Essential', name:'gu.mock'});
+        expect(cookieValueDefault).toEqual('someTestData');
+        await cmpSetCookie({useCase: 'Essential', name: 'gu.mock', value: 'testdata'});
+		const cookieValue = await cmpGetCookie({useCase: 'Essential', name: 'gu.mock'});
+        expect(cookieValue).toEqual('testdata');
+    });
+	test('get null if cookie does not exist', async () => {
+        const consentState: ConsentState = {
+            tcfv2: tcfv2ConsentStateNoConsent,
+            canTarget: false,
+            framework: 'tcfv2',
+        };
+        mockOnConsentChange(consentState);
+		const cookieValue = await cmpGetCookie({useCase: 'Essential', name: 'gu.does_not_exist'});
+        expect(cookieValue).toEqual(null);
+    });
 });
