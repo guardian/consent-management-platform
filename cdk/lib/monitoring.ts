b2eae3c5325aed18f006b6c2dc0fea1f77b77464
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-12-20 10:59:24 +0000

    Update monitoring.ts

--- cdk/lib/monitoring.ts
+++ cdk/lib/monitoring.ts
@@ -94,10 +94,10 @@ export class Monitoring extends GuStack {
 		const alarm = new Alarm(this, 'cmp-monitoring-alarms', {
 			comparisonOperator:
 				ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
-			threshold: 4,
+			threshold: 1,
 			evaluationPeriods: 5, //10 // This value is the number of periods to watch. Here, we're evaluating 5 executions of the lambda.
 			actionsEnabled: true,
-			datapointsToAlarm: 1, //5 // This value is the number of failed data-points/executions that will trigger the alarm. so 4 out of 5
+			datapointsToAlarm: 4, //5 // This value is the number of failed data-points/executions that will trigger the alarm. so 4 out of 5
 			treatMissingData: TreatMissingData.NOT_BREACHING,
 			metric: errorMetric,
 			alarmName: `CMP Monitoring - ${stage} - ${region}`,
