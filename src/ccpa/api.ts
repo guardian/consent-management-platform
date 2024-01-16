cd0941341c760bbbfb20e39315c73ab670288296
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-01-16 15:43:22 +0000

    Removing gpp

--- src/ccpa/api.ts
+++ src/ccpa/api.ts
@@ -1,9 +1,8 @@
-import type { CCPAData, GPPData } from '../types/ccpa';
+import type { CCPAData } from '../types/ccpa';
 
-type UspApiCommand = 'getUSPData';
-type GppCommand = 'ping';
+type Command = 'getUSPData';
 
-const uspApi = (command: UspApiCommand) =>
+const api = (command: Command) =>
 	new Promise((resolve, reject) => {
 		if (window.__uspapi) {
 			window.__uspapi(command, 1, (result, success) =>
@@ -17,22 +16,5 @@ const uspApi = (command: UspApiCommand) =>
 		}
 	});
 
-const gppApi = (command: GppCommand) =>
-	new Promise((resolve, reject) => {
-		if (window.__gpp) {
-			window.__gpp(command, (result, success) =>
-				success
-					? resolve(result)
-					: /* istanbul ignore next */
-					  reject(new Error(`Unable to get ${command} data`)),
-			);
-		} else {
-			reject(new Error('No __gpp found on window'));
-		}
-	});
-
-export const getGPPData = (): Promise<GPPData> =>
-	gppApi('ping') as Promise<GPPData>;
-
 export const getUSPData = (): Promise<CCPAData> =>
-	uspApi('getUSPData') as Promise<CCPAData>;
+	api('getUSPData') as Promise<CCPAData>;
--- src/ccpa/getConsentState.ts
+++ src/ccpa/getConsentState.ts
@@ -1,11 +1,9 @@
 import type { CCPAConsentState } from '../types/ccpa';
-import { getGPPData, getUSPData } from './api';
+import { getUSPData } from './api';
 
 // get the current consent state using the official IAB method
 export const getConsentState: () => Promise<CCPAConsentState> = async () => {
 	const uspData = await getUSPData();
-	const gppData = await getGPPData();
-
 
 	// TODO: GET GPP DATA AND COMPARE
 	return {
