2b34685e5bd3add424c97d05e32ce622c0148a15
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-01-16 15:21:49 +0000

    using getGPPData

--- src/ccpa/getConsentState.ts
+++ src/ccpa/getConsentState.ts
@@ -1,10 +1,13 @@
 import type { CCPAConsentState } from '../types/ccpa';
-import { getUSPData } from './api';
+import { getGPPData, getUSPData } from './api';
 
 // get the current consent state using the official IAB method
 export const getConsentState: () => Promise<CCPAConsentState> = async () => {
 	const uspData = await getUSPData();
+	const gppData = await getGPPData();
 
+
+	// TODO: GET GPP DATA AND COMPARE
 	return {
 		doNotSell: uspData.uspString.charAt(2) === 'Y',
 	};
