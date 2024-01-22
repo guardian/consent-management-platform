0f8b4bfb2886639924259ee3074711724ff88460
Anna Voelker <anna.voelker@guardian.co.uk>
2024-01-22 21:12:43 +0000

    add "No consent required"

--- src/types/consentUseCases.ts
+++ src/types/consentUseCases.ts
@@ -4,11 +4,13 @@
  * - `Targeted advertising`: if the user can be targeted for personalised advertising according to the active consent framework
  * - 'Targeted marketing': if the user can be targeted for personalised marketing, e.g. article count
  * - `Essential`: if essential cookies/storage can be used according to the active consent framework
+ * - `No Consent Required`: does not do any consent checks. This is almost certainly not the right choice. Please speak to T&C before using it.
  *
  */
 export const ConsentUseCaseOptions = [
 	"Targeted advertising",
 	"Targeted marketing",
-	"Essential"
+	"Essential",
+	"No consent required"
 ] as const;
 export type ConsentUseCases = typeof ConsentUseCaseOptions[number];
