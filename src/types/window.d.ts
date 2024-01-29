60b7d1501572d84749df9b20299fa44cbdfa674d
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-01-29 10:50:19 +0100

    chore: Enable GPP Multi-State Privacy String (MSPS) with U.S. Privacy (#806)
    * Create stub_gpp_ccpa.js
    
    * Added stub_gpp_ccpa
    
    * Added reference
    
    * Added includeGppApi
    
    * Update stub.ts
    
    * Momentarily committing a changeset
    
    * Momentarily log
    
    * Fixing bug
    
    * Update sourcepoint.ts
    
    * Update sourcepoint.ts
    
    * Update sourcepoint.ts
    
    * Updated file name to stub_uspapi_ccpa
    
    * Update early-rocks-destroy.md
    
    * Update for aus and added _gpp
    
    * Update sourcepoint.test.js
    
    * Added GPPData interface
    
    * Create new function to get gpp data
    
    * using getGPPData
    
    * Removing gpp
    
    * Update getConsentState.ts
    
    * Using GPPData
    
    * Updated to minor
    
    * Using tcfv2 in unknown frameworks
    
    * Fixed test
    
    * Update stub.ts
    
    * Update sourcepoint.ts

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -3,7 +3,7 @@ import type { Property } from '../lib/property';
 import type { EndPoint } from '../lib/sourcepointConfig';
 import type { onConsent } from '../onConsent';
 import type { onConsentChange } from '../onConsentChange';
-import type { CCPAData } from './ccpa';
+import type { CCPAData, GPPData } from './ccpa';
 import type { TCData } from './tcfv2/TCData';
 import type { CMP, Framework, PubData } from '.';
 
@@ -35,6 +35,7 @@ declare global {
 					framework: Framework;
 				};
 				ccpa?: {
+					includeGppApi?: boolean;
 					targetingParams?: {
 						framework: Framework;
 					};
@@ -96,6 +97,10 @@ declare global {
 			version: number,
 			callback: (tcData: TCData, success: boolean) => void,
 			vendorIDs?: number[],
+		) => void;
+		__gpp?: (
+			command: string,
+			callback: (gppData: GPPData, success: boolean) => void,
 		) => void;
 	}
 }
