215684e3f02b20b2d44e04c7f86d3b981bd747a8
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-03-12 15:33:01 +0000

    Update getConsentState.test.js

--- src/ccpa/getConsentState.test.js
+++ src/ccpa/getConsentState.test.js
@@ -1,14 +1,30 @@
+import gppDataFail from './__fixtures__/api.getGPPData.fail.json';
+import gppDataSuccess from './__fixtures__/api.getGPPData.success.json';
 import uspData from './__fixtures__/api.getUSPData.json';
-import { getUSPData } from './api.ts';
+import { getGPPData, getUSPData } from './api.ts';
 import { getConsentState } from './getConsentState.ts';
 
 jest.mock('./api');
 getUSPData.mockResolvedValue(uspData);
+// getGPPData.mockResolvedValue(gppDataSuccess);
 
 describe('getConsentState', () => {
-	it('gets the consent state correctly', async () => {
+	it('gets the gpp consent state correctly', async () => {
+		getGPPData.mockResolvedValue(gppDataSuccess);
+
+		const { doNotSell } = await getConsentState();
+		expect(getGPPData).toHaveBeenCalledTimes(1);
+		expect(getUSPData).toHaveBeenCalledTimes(0);
+
+		expect(doNotSell).toBe(false);
+	});
+
+	it('gets the usp consent state correctly', async () => {
+		getGPPData.mockResolvedValue(gppDataFail);
+
 		const { doNotSell } = await getConsentState();
 
+		expect(getGPPData).toHaveBeenCalledTimes(1);
 		expect(getUSPData).toHaveBeenCalledTimes(1);
 		expect(doNotSell).toBe(true);
 	});
