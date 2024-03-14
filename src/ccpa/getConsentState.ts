34ba3b1cdf17e98d87b42b1af7b04b4afa6f39f2
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-03-14 11:34:18 +0000

    Remove getUSPData

--- src/ccpa/getConsentState.ts
+++ src/ccpa/getConsentState.ts
@@ -1,19 +1,24 @@
 import type { CCPAConsentState } from '../types/ccpa';
-import { getGPPData, getUSPData } from './api';
+import { getGPPData } from './api';
 
 // get the current consent state using the official IAB method
 export const getConsentState: () => Promise<CCPAConsentState> = async () => {
-	const gppData = await getGPPData()
+	const gppData = await getGPPData();
 
-	if(gppData.parsedSections && gppData.applicableSections.length > 0 && gppData.applicableSections.includes(7)) {
-		return {
-			doNotSell: gppData.parsedSections.usnatv1.SaleOptOut !== 2
-		}
+	if (
+		gppData.parsedSections &&
+		gppData.applicableSections.length > 0 &&
+		gppData.applicableSections.includes(7)
+	) {
+		console.log('GPP DATA', gppData);
+		console.log(
+			'doNotSell',
+			gppData.parsedSections.usnatv1.SaleOptOut !== 2,
+		);
 	}
-
-	const uspData = await getUSPData();
-
 	return {
-		doNotSell: uspData.uspString.charAt(2) === 'Y',
+		doNotSell: gppData.parsedSections
+			? gppData.parsedSections.usnatv1.SaleOptOut !== 2
+			: true,
 	};
 };
