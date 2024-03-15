9954dfadaebf5b43d601a4291a7f9d85d8cdc1c5
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-03-15 10:54:09 +0000

    cleaned up getConsentState

--- src/ccpa/getConsentState.ts
+++ src/ccpa/getConsentState.ts
@@ -5,20 +5,7 @@ import { getGPPData } from './api';
 export const getConsentState: () => Promise<CCPAConsentState> = async () => {
 	const gppData = await getGPPData();
 
-	if (
-		gppData.parsedSections &&
-		gppData.applicableSections.length > 0 &&
-		gppData.applicableSections.includes(7)
-	) {
-		console.log('GPP DATA', gppData);
-		console.log(
-			'doNotSell',
-			gppData.parsedSections.usnatv1.SaleOptOut !== 2,
-		);
-	}
 	return {
-		doNotSell: gppData.parsedSections
-			? gppData.parsedSections.usnatv1.SaleOptOut !== 2
-			: true,
+		doNotSell: gppData.parsedSections.usnatv1?.SaleOptOut !== 2,
 	};
 };
