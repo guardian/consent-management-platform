634957e0338906d462db95b69cf6e61a2db62c62
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2024-06-11 12:49:16 +0100

    Add amp article

--- monitoring/src/types.ts
+++ monitoring/src/types.ts
@@ -31,7 +31,7 @@ export const ELEMENT_ID = {
 	TCFV2_FIRST_LAYER_MANAGE_COOKIES:
 		'div.message-component.message-row > button.sp_choice_type_12',
 	TOP_ADVERT: '.ad-slot--top-above-nav .ad-slot__content',
-	CMP_CONTAINER: '[id*="sp_message_iframe"]',
+	CMP_CONTAINER: 'iframe[id*="sp_message_iframe"]',
 	TCFV2_SECOND_LAYER_SAVE_AND_EXIT: 'button.sp_choice_type_SAVE_AND_EXIT',
 	TCFV2_SECOND_LAYER_HEADLINE: 'p.gu-privacy-headline',
 	CCPA_DO_NOT_SELL_BUTTON: 'div.message-component > button.sp_choice_type_13',
@@ -58,6 +58,7 @@ export type Config = {
 	jurisdiction: Jurisdiction;
 	region: AwsRegionOpt;
 	frontUrl: string;
+	ampArticle: string;
 	articleUrl: string;
 	iframeDomain: string;
 	iframeDomainSecondLayer: string;
