ac308cdacdb90db3799a408ef24513eddf8e29a4
Anna Voelker <anna.voelker@guardian.co.uk>
2024-07-10 14:38:47 +0100

    remove manual runtime management

--- cdk/lib/monitoring.ts
+++ cdk/lib/monitoring.ts
@@ -16,7 +16,7 @@ import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
 import { Rule, RuleTargetInput, Schedule } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { Runtime, RuntimeManagementMode } from 'aws-cdk-lib/aws-lambda';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Topic } from 'aws-cdk-lib/aws-sns';
 import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 
@@ -30,8 +30,6 @@ export class Monitoring extends GuStack {
 
 		const lambdaBaseName = 'cmp-monitoring';
 
-		const runTimeId = "0cdcfbdefbc5e7d3343f73c2e2dd3cba17d61dea0686b404502a0c9ce83931b9";
-
 		const prodDurationInMinutes = 2;
 
 		const policyStatement = new PolicyStatement({
@@ -39,8 +37,6 @@ export class Monitoring extends GuStack {
 			actions: ['cloudwatch:PutMetricData'],
 			resources: ['*'],
 		});
-
-		const runTimeManagementArn = `arn:aws:lambda:${region}::runtime:${runTimeId}`
 
 		const monitoringLambdaFunction = new GuLambdaFunction(
 			this,
@@ -51,7 +47,6 @@ export class Monitoring extends GuStack {
 				fileName: `${lambdaBaseName}-lambda-${region}.zip`,
 				handler: 'index.handler',
 				runtime: Runtime.NODEJS_20_X,
-				runtimeManagementMode: RuntimeManagementMode.manual(runTimeManagementArn),
 				timeout: Duration.seconds(300),
 				memorySize: 2048,
 				initialPolicy: [policyStatement],
