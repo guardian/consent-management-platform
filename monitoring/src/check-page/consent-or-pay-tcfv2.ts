24d8d8959cb40d99134e5cb63485de351044585b
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-03-12 15:38:45 +0000

    Removed commented out code. Restructured code.

--- monitoring/src/check-page/consent-or-pay-tcfv2.ts
+++ monitoring/src/check-page/consent-or-pay-tcfv2.ts
@@ -6,30 +6,21 @@ import {
 	ELEMENT_ID,
 } from '../types';
 import {
-	// checkAds,
 	checkCMPIsNotVisible,
 	checkCMPIsOnPage,
-	// checkCMPLoadingTimeAndVersion,
-	// checkGoogleAdManagerRequestIsMade,
-	// checkPrivacySettingsPanelIsOpen,
 	checkTopAdDidNotLoad,
 	checkTopAdHasLoaded,
 	clearCookies,
 	clearLocalStorage,
 	clickAcceptAllCookies,
 	clickBannerButton,
-	// clickRejectAll,
-	// clickRejectAllSecondLayer,
-	// clickSaveAndCloseSecondLayer,
 	dropCookiesForNonAdvertisingBanner,
-	// dropCookiesForSignedInUser,
 	loadPage,
 	log_info,
 	log_line,
 	makeNewBrowser,
 	makeNewContext,
 	makeNewPage,
-	// openPrivacySettingsPanel,
 	reloadPage,
 } from './common-functions';
 
@@ -41,58 +32,21 @@ const BannerType = {
 
 type Banner = (typeof BannerType)[keyof typeof BannerType];
 
+const checkOptOutLoads = async (page: Page) => {
+	log_info('Checking opt out loads');
+	await page.waitForRequest(/cdn\.optoutadvertising\.com/);
+	log_info('Checked opt out loads');
+};
+
 const isUsingNonPersonalisedAds = async (page: Page): Promise<void> => {
 	await checkTopAdDidNotLoad(page);
 	// TODO: Check the optout advertising url is called
+	await checkOptOutLoads(page);
 };
 
 const isUsingPersonalisedAds = async (page: Page): Promise<void> => {
 	await checkTopAdHasLoaded(page);
 	// TODO: Check opt out is not loaded.
-};
-
-/**
- * Checks that ads load correctly for the second page a user goes to
- * when visiting the site, with respect to and interaction with the CMP.
- *
- * @param {BrowserContext} context
- * @param {Config} config
- * @param {string} url
- */
-const checkSubsequentPage = async (
-	context: BrowserContext,
-	config: Config,
-	url: string,
-	bannerType: Banner,
-) => {
-	log_info(`Start checking subsequent Page URL: ${url}`);
-	const page: Page = await makeNewPage(context);
-	await loadPage(page, url);
-	// There is no CMP since this we have already accepted this on a previous page.
-	await checkCMPIsNotVisible(page);
-
-	switch (bannerType) {
-		case BannerType.CONSENT_OR_PAY_SIGNED_IN:
-			await isUsingPersonalisedAds(page);
-			break;
-		case BannerType.CONSENT_OR_PAY_SIGNED_OUT:
-			await isUsingPersonalisedAds(page);
-			break;
-		case BannerType.NON_ADVERTISING:
-			await isUsingNonPersonalisedAds(page);
-			break;
-	}
-
-	await Promise.all([clearCookies(page), clearLocalStorage(page)]);
-	await reloadPage(page);
-	await isUsingNonPersonalisedAds(page);
-	await clickAcceptAllCookies(config, page, "Yes I'm Happy");
-	await Promise.all([
-		checkCMPIsNotVisible(page),
-		isUsingPersonalisedAds(page),
-	]);
-	await page.close();
-	log_info(`Checking subsequent Page URL: ${url} Complete`);
 };
 
 /**
@@ -120,6 +74,95 @@ const setupPage = async (
 		browser,
 		context,
 	};
+};
+
+/**
+ * This checks that the sign in link is on the CMP
+ *
+ * @param {Page} page
+ */
+const checkSignInLinkIsOnCMP = async (page: Page) => {
+	(
+		await page
+			.frameLocator(ELEMENT_ID.CMP_CONTAINER)
+			.locator(ELEMENT_ID.CMP_ACTIONS_ROW)
+			.allInnerTexts()
+	).includes('sign in');
+};
+
+/**
+ * This checks that the user was redirected to the guardian ad lite page
+ *
+ * @param {Page} page
+ */
+const checkWasRedirectedToGuardianLite = async (page: Page) => {
+	await page.waitForURL('**/guardian-ad-lite?returnAddress=*');
+};
+
+/**
+ * User who is paying for a subscription that allows them to view the site without google ad manager and
+ * with a non-advertising banner.
+ *
+ * @param {Config} config
+ * @param {string} url
+ * @param {string} nextUrl
+ */
+const checkConsentOrPayBanner = async (
+	config: Config,
+	url: string,
+	nextUrl: string,
+	bannerInteraction: BannerInteraction,
+	bannerType: Banner,
+) => {
+	if (bannerType === BannerType.CONSENT_OR_PAY_SIGNED_IN) {
+		log_info('checking pages for Consent or Pay banner (signed in)');
+	}
+
+	if (bannerType === BannerType.CONSENT_OR_PAY_SIGNED_OUT) {
+		log_info('checking pages for Consent or Pay banner (signed out)');
+	}
+
+	const { browser, page, context } = await setupPage(config, bannerType);
+
+	await loadPage(page, url);
+
+	await checkCMPIsOnPage(page);
+
+	if (bannerType === BannerType.CONSENT_OR_PAY_SIGNED_OUT) {
+		await checkSignInLinkIsOnCMP(page);
+	}
+
+	await checkTopAdDidNotLoad(page);
+
+	switch (bannerInteraction) {
+		case BannerInteractions.ACCEPT_ALL:
+			await clickBannerButton(
+				page,
+				'Accept all',
+				BannerInteractions.ACCEPT_ALL,
+			);
+
+			await checkCMPIsNotVisible(page);
+
+			await isUsingPersonalisedAds(page);
+			break;
+		case BannerInteractions.REJECT_AND_SUBSCRIBE:
+			await clickBannerButton(
+				page,
+				'Reject and Subscribe',
+				BannerInteractions.REJECT_AND_SUBSCRIBE,
+			);
+
+			await checkWasRedirectedToGuardianLite(page);
+			break;
+	}
+
+	if (nextUrl) {
+		await checkSubsequentPage(context, config, nextUrl, bannerType);
+	}
+
+	await page.close();
+	await browser.close();
 };
 
 /**
@@ -185,82 +228,48 @@ const checkNonAdvertisingBanner = async (
 	await browser.close();
 };
 
-const checkSignInLinkIsOnCMP = async (page: Page) => {
-	((await page.frameLocator(ELEMENT_ID.CMP_CONTAINER).locator(ELEMENT_ID.CMP_ACTIONS_ROW).allInnerTexts()).includes('sign in'))
-};
-
-const checkWasRedirectedToGuardianLite = async (page: Page) => {
-	await page.waitForURL('**/guardian-ad-lite?returnAddress=*');
-};
-
 /**
- * User who is paying for a subscription that allows them to view the site without google ad manager and
- * with a non-advertising banner.
+ * Checks that ads load correctly for the second page a user goes to
+ * when visiting the site, with respect to and interaction with the CMP.
  *
+ * @param {BrowserContext} context
  * @param {Config} config
  * @param {string} url
- * @param {string} nextUrl
  */
-const checkConsentOrPayBanner = async (
+const checkSubsequentPage = async (
+	context: BrowserContext,
 	config: Config,
 	url: string,
-	nextUrl: string,
-	bannerInteraction: BannerInteraction,
 	bannerType: Banner,
 ) => {
-	if(bannerType === BannerType.CONSENT_OR_PAY_SIGNED_IN){
-		log_info('checkPage Consent or Pay banner (signed in)');
-	}
-
-	if(bannerType === BannerType.CONSENT_OR_PAY_SIGNED_OUT){
-		log_info('checkPage Consent or Pay banner (signed out)');
-	}
-
-	const { browser, page, context } = await setupPage(config, bannerType);
-
+	log_info(`Start checking subsequent Page URL: ${url}`);
+	const page: Page = await makeNewPage(context);
 	await loadPage(page, url);
+	// There is no CMP since this we have already accepted this on a previous page.
+	await checkCMPIsNotVisible(page);
 
-	await checkCMPIsOnPage(page);
-
-	if(bannerType === BannerType.CONSENT_OR_PAY_SIGNED_OUT){
-		await checkSignInLinkIsOnCMP(page);
+	switch (bannerType) {
+		case BannerType.CONSENT_OR_PAY_SIGNED_IN:
+			await isUsingPersonalisedAds(page);
+			break;
+		case BannerType.CONSENT_OR_PAY_SIGNED_OUT:
+			await isUsingPersonalisedAds(page);
+			break;
+		case BannerType.NON_ADVERTISING:
+			await isUsingNonPersonalisedAds(page);
+			break;
 	}
 
+	await Promise.all([clearCookies(page), clearLocalStorage(page)]);
+	await reloadPage(page);
 	await checkTopAdDidNotLoad(page);
-
-	switch (bannerInteraction) {
-		case BannerInteractions.ACCEPT_ALL:
-			await clickBannerButton(
-				page,
-				'Accept all',
-				BannerInteractions.ACCEPT_ALL,
-			);
-
-			await checkCMPIsNotVisible(page);
-
-			await isUsingNonPersonalisedAds(page);
-			break;
-		case BannerInteractions.REJECT_AND_SUBSCRIBE:
-			await clickBannerButton(
-				page,
-				'Reject and Subscribe',
-				BannerInteractions.REJECT_AND_SUBSCRIBE,
-			);
-
-			await checkWasRedirectedToGuardianLite(page);
-
-			await checkCMPIsNotVisible(page);
-
-			await isUsingNonPersonalisedAds(page);
-			break;
-	}
-
-	if (nextUrl) {
-		await checkSubsequentPage(context, config, nextUrl, bannerType);
-	}
-
+	await clickAcceptAllCookies(config, page, "Yes I'm Happy");
+	await Promise.all([
+		checkCMPIsNotVisible(page),
+		isUsingPersonalisedAds(page),
+	]);
 	await page.close();
-	await browser.close();
+	log_info(`Checking subsequent Page URL: ${url} Complete`);
 };
 
 /**
@@ -273,10 +282,6 @@ const checkConsentOrPayBanner = async (
  * @return {*}  {Promise<void>}
  */
 export const mainCheck = async function (config: Config): Promise<void> {
-	log_info('checkPage Consent or pay (tcfv2)');
-
-	log_line();
-
 	await checkConsentOrPayBanner(
 		config,
 		`${config.frontUrl}?adtest=fixed-puppies`,
@@ -320,7 +325,6 @@ export const mainCheck = async function (config: Config): Promise<void> {
 		BannerInteractions.ACCEPT_ALL,
 	);
 	log_line();
-
 
 	await checkNonAdvertisingBanner(
 		config,
@@ -329,5 +333,4 @@ export const mainCheck = async function (config: Config): Promise<void> {
 		BannerInteractions.REJECT_ALL,
 	);
 	log_line();
-
 };
