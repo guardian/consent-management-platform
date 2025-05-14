231b37e734c891a006cdebabcb97a4c759d68675
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2025-05-14 09:10:43 +0100

    Comment recommendations

--- synthetics/src/main.js
+++ synthetics/src/main.js
@@ -1,34 +1,27 @@
 import { ConfigWrapper } from "./utils/config/config-wrapper.js";
+import { Log } from "./utils/log.js";
 
 export const main = async (browserType, region, stage) => {
 	try {
-
 		const configWrapper = new ConfigWrapper(
 			region.toLowerCase(),
 			stage.toLowerCase(),
 			null,
 		);
 		configWrapper.generateConfig();
-		console.log("Config generated:", configWrapper.config);
+		Log.info("Config generated:", configWrapper.config);
 
-		try {
-			await configWrapper.run(browserType);
-		} catch (error) {
-			let errorMessage = "Unknown Error!";
-
-			if (typeof error === "string") {
-				errorMessage = error;
-			} else if (error instanceof Error) {
-				errorMessage = error.message;
-			}
-
-			console.log(
-				`(cmp monitoring) Finished with failure: ${errorMessage}`,
-			);
-
-			throw new Error(errorMessage);
-		}
+		await configWrapper.run(browserType);
 	} catch (error) {
+		let errorMessage = "Unknown Error!";
+
+		if (typeof error === "string") {
+			errorMessage = error;
+		} else if (error instanceof Error) {
+			errorMessage = error.message;
+		}
+
+		Log.error(`Finished with failure: ${errorMessage}`);
 		console.error("Error in handler:", error);
 		throw error; // Rethrow the error to ensure the Lambda function fails
 	} finally {
