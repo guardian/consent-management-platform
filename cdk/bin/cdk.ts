80e7308241cf895177d5b76ac53289610258bb02
AshCorr <ashcorr20@gmail.com>
2025-06-26 14:37:06 +0100

    feat(cdk): Use latest standard Guardian CDK setup (#1176)

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -18,10 +18,13 @@ const deployableEnvs = ['CODE', 'PROD'] as const;
 type DeployableEnvironments = (typeof deployableEnvs)[number];
 
 function toPascalCase(input: string): string {
-    return input
-        .split('-')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-        .join('');
+	return input
+		.split('-')
+		.map(
+			(word) =>
+				word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
+		)
+		.join('');
 }
 
 function stackProps(
